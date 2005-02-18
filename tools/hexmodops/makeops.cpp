5b5061d83f6a4c44bac3356b8f514e6c0c4e1657
Tim Tautges <tautges@mcs.anl.gov>
2005-02-18 16:46:31 +0000

    Adding code to construct various configurations for studying hex modification.
    git-svn-id: https://svn.mcs.anl.gov/repos/ITAPS/MOAB/trunk@591 6162379e-bd28-0410-9a7d-b7f4dcfcad3c

---
+++ tools/hexmodops/makeops.cpp
@@ -0,0 +1,352 @@
+/*
+ * Program to make hex modification operation meshes
+ *
+ */
+
+#include "MBCore.hpp"
+#include "MBRange.hpp"
+#include <iostream>
+
+MBInterface *gMB = NULL;
+
+MBErrorCode make_atomic_pillow();
+MBErrorCode make_face_shrink();
+MBErrorCode make_face_open_collapse();
+MBErrorCode make_chord_push();
+MBErrorCode make_triple_chord_push();
+MBErrorCode make_triple_hex_push();
+
+enum OperationType {ATOMIC_PILLOW = 0, 
+                    FACE_OPEN_COLLAPSE, 
+                    FACE_SHRINK, 
+                    CHORD_PUSH, 
+                    TRIPLE_CHORD_PUSH, 
+                    TRIPLE_HEX_PUSH,
+                    UNDEFINED};
+
+const char *OperationNames[] = {"atomic_pillow", 
+                                "face_open_collapse", 
+                                "face_shrink", 
+                                "chord_push", 
+                                "triple_chord_push", 
+                                "triple_hex_push",
+                                "undefined"};
+
+int main(int argc, char **argv) 
+{
+  gMB = new MBCore();
+  const char *extensions[] = 
+    {
+      ".g",
+      ".h5m",
+      ".vtk"
+    };
+  int file_exten = 1;
+  
+  std::vector<OperationType> op_types;
+
+  if (argc < 2) {
+    std::cout << "Usage: " << argv[0] 
+              << " [-h5m] [-vtk] {-ap | -foc | -fs | -cp | -tcp | -thp}" 
+              << std::endl;
+    return 1;
+  }
+    
+  int current_arg = 1;
+  while (current_arg < argc) {
+    if (!strcmp("-g", argv[current_arg])) file_exten = 0;
+    else if (!strcmp("-h5m", argv[current_arg])) file_exten = 1;
+    else if (!strcmp("-vtk", argv[current_arg])) file_exten = 2;
+    else if (!strcmp("-ap", argv[current_arg])) 
+      op_types.push_back(ATOMIC_PILLOW);
+    else if (!strcmp("-foc", argv[current_arg])) 
+      op_types.push_back(FACE_OPEN_COLLAPSE);
+    else if (!strcmp("-fs", argv[current_arg])) 
+      op_types.push_back(FACE_SHRINK);
+    else if (!strcmp("-cp", argv[current_arg])) 
+      op_types.push_back(CHORD_PUSH);
+    else if (!strcmp("-tcp", argv[current_arg])) 
+      op_types.push_back(TRIPLE_CHORD_PUSH);
+    else if (!strcmp("-thp", argv[current_arg])) 
+      op_types.push_back(TRIPLE_HEX_PUSH);
+    current_arg++;
+  }
+  
+  MBErrorCode result = MB_SUCCESS, tmp_result = MB_FAILURE;
+
+  for (std::vector<OperationType>::iterator vit = op_types.begin(); 
+       vit != op_types.end(); vit++) {
+    if (*vit == ATOMIC_PILLOW) {
+      tmp_result = make_atomic_pillow();
+    }
+    else if (*vit == FACE_OPEN_COLLAPSE) {
+      tmp_result = make_face_open_collapse();
+    }
+    else if (*vit == CHORD_PUSH) {
+      tmp_result = make_chord_push();
+    }
+    else if (*vit == TRIPLE_CHORD_PUSH) {
+      tmp_result = make_triple_chord_push();
+    }
+
+    else if (*vit == TRIPLE_HEX_PUSH) {
+      tmp_result = make_triple_hex_push();
+    }
+    else if (*vit == FACE_SHRINK) {
+      tmp_result = make_face_shrink();
+    }
+    else {
+      std::cout << "Operation undefined." << std::endl;
+      return 1;
+    }
+  
+    // now write to a file
+    std::string filename(OperationNames[*vit]);
+    filename.append(extensions[file_exten]);
+    tmp_result = gMB->write_mesh(filename.c_str());
+    if (MB_SUCCESS != tmp_result) result = tmp_result;
+  }
+    
+  return (result == MB_SUCCESS ? 0 : 1);
+}
+
+MBErrorCode make_atomic_pillow() 
+{
+    // make atomic pillow configuration
+    // make all vertices
+  double vtx_coord[] = 
+    {
+      1.0, 1.0, 1.0, 
+      1.0, 0.0, 1.0, 
+      0.0, 0.0, 1.0, 
+      0.0, 1.0, 1.0, 
+      .75, .75, 1.0, 
+      .75, .25, 1.0, 
+      .25, .25, 1.0, 
+      .25, .75, 1.0
+    };
+
+  int connect[] = {
+    0, 1, 2, 3, 4, 5, 6, 7,
+    4, 5, 6, 7, 0, 1, 2, 3
+  };
+
+  MBErrorCode result;
+  MBEntityHandle vtx_handles[8];
+  
+  for (int i = 0; i < 8; i++) {
+    result = gMB->create_vertex(&vtx_coord[3*i], vtx_handles[i]);
+    if (MB_SUCCESS != result) return MB_FAILURE;
+  }
+  
+  MBEntityHandle conn[8], elems[4];
+
+    // make the two hexes
+  for (int i = 0; i < 8; i++)
+    conn[i] = vtx_handles[connect[i]];
+  result = gMB->create_element(MBHEX, conn, 8, elems[0]);
+  if (MB_SUCCESS != result) return MB_FAILURE;
+
+  for (int i = 0; i < 8; i++)
+    conn[i] = vtx_handles[connect[8+i]];
+  result = gMB->create_element(MBHEX, conn, 8, elems[1]);
+  if (MB_SUCCESS != result) return MB_FAILURE;
+
+    // make one of the end quads explicitly and bind to the first hex
+  for (int i = 0; i < 4; i++)
+    conn[i] = vtx_handles[connect[i]];
+  result = gMB->create_element(MBQUAD, conn, 4, elems[2]);
+  if (MB_SUCCESS != result) return MB_FAILURE;
+  
+  result = gMB->add_adjacencies(elems[2], elems, 1, false);
+  if (MB_SUCCESS != result) return MB_FAILURE;
+
+    // now the other one
+  result = gMB->create_element(MBQUAD, conn, 4, elems[3]);
+  if (MB_SUCCESS != result) return MB_FAILURE;
+  
+  result = gMB->add_adjacencies(elems[3], &elems[1], 1, false);
+  if (MB_SUCCESS != result) return MB_FAILURE;
+
+  return MB_SUCCESS;
+}
+
+MBErrorCode make_face_shrink() 
+{
+    // make face shrink configuration
+    // make all vertices
+  double vtx_coord[] = 
+    {
+      1.0, 1.0, 0.0, 
+      1.0, 0.0, 0.0, 
+      0.0, 0.0, 0.0, 
+      0.0, 1.0, 0.0, 
+      1.0, 1.0, 1.0, 
+      1.0, 0.0, 1.0, 
+      0.0, 0.0, 1.0, 
+      0.0, 1.0, 1.0, 
+      1.0, 1.0, 2.0, 
+      1.0, 0.0, 2.0, 
+      0.0, 0.0, 2.0, 
+      0.0, 1.0, 2.0, 
+      .75, .75, 1.0, 
+      .75, .25, 1.0, 
+      .25, .25, 1.0, 
+      .25, .75, 1.0
+    };
+
+  int connect[] = {
+    3, 7, 11, 15, 0, 4, 8, 12,
+    0, 4, 8, 12, 1, 5, 9, 13,
+    1, 5, 9, 13, 2, 6, 10, 14,
+    2, 6, 10, 14, 3, 7, 11, 15,
+    0, 3, 2, 1, 12, 15, 14, 13,
+    12, 15, 14, 13, 8, 11, 10, 9
+  };
+
+  MBErrorCode result;
+  MBEntityHandle vtx_handles[16];
+  
+  for (int i = 0; i < 16; i++) {
+    result = gMB->create_vertex(&vtx_coord[3*i], vtx_handles[i]);
+    if (MB_SUCCESS != result) return MB_FAILURE;
+  }
+  
+    // make all elements at once
+  MBEntityHandle conn[8], elems[6];
+
+  for (int j = 0; j < 6; j++) {
+    for (int i = 0; i < 8; i++)
+      conn[i] = vtx_handles[connect[j*8+i]];
+    
+    result = gMB->create_element(MBHEX, conn, 8, elems[j]);
+    if (MB_SUCCESS != result) return MB_FAILURE;
+  }
+  
+  return MB_SUCCESS;
+}
+
+MBErrorCode make_face_open_collapse() 
+{
+  return MB_FAILURE;
+}
+
+MBErrorCode make_chord_push() 
+{
+    // make chord push configuration
+    // make all vertices
+  double vtx_coord[] = 
+    {
+        // first layer
+      0.0, 0.0, 0.5,
+      0.0, 1.0, 0.0,
+      -1.0, 0.5, 0.0, 
+      -1.0, -0.5, 0.0,
+      0.0, -1.0, 0.0,
+      1.0, -0.5, 0.0,
+      1.0, 0.5, 0.0,
+        // second layer
+      0.0, 0.0, -1.5,
+      0.0, 1.0, -1.0,
+      -1.0, 0.5, -1.0, 
+      -1.0, -0.5, -1.0,
+      0.0, -1.0, -1.0,
+      1.0, -0.5, -1.0,
+      1.0, 0.5, -1.0,
+        // 2 extra vertices for chord push
+      0.0, -.333, 0.0,
+      0.0, -.667, 0.0
+    };
+
+  int connect[] = {
+      // 3 "normal" hexes first
+      // top hex
+    0, 2, 1, 6, 7, 9, 8, 13,
+      // bottom left
+    0, 4, 3, 2, 7, 11, 10, 9,
+      // bottom right
+    6, 5, 4, 0, 13, 12, 11, 7,
+      // front chord push hex
+    2, 0, 4, 3, 14, 6, 5, 15,
+      // back chord push hex
+    2, 14, 15, 3, 0, 6, 5, 4,
+      // front/rear quads a, b
+    2, 0, 4, 3, 6, 5, 4, 0,
+      // duplicate edges from chord push
+    0, 4
+  };
+
+  MBErrorCode result;
+  MBEntityHandle vtx_handles[16];
+  
+  for (int i = 0; i < 6; i++) {
+    result = gMB->create_vertex(&vtx_coord[3*i], vtx_handles[i]);
+    if (MB_SUCCESS != result) return MB_FAILURE;
+  }
+  
+  MBEntityHandle conn[8], elems[10];
+
+    // make the five hexes
+  for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < 8; j++)
+      conn[i] = vtx_handles[connect[8*i+j]];
+    result = gMB->create_element(MBHEX, conn, 8, elems[i]);
+    if (MB_SUCCESS != result) return MB_FAILURE;
+  }
+
+    // make the frontmost pair of quads and bind to the front degen hex
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 4; j++)
+      conn[i] = vtx_handles[connect[40+4*i+j]];
+    result = gMB->create_element(MBQUAD, conn, 4, elems[5+i]);
+    if (MB_SUCCESS != result) return MB_FAILURE;
+  }
+  
+    // now the back pair
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 4; j++)
+      conn[i] = vtx_handles[connect[40+4*i+j]];
+    result = gMB->create_element(MBQUAD, conn, 4, elems[7+i]);
+    if (MB_SUCCESS != result) return MB_FAILURE;
+  }
+
+    // make the duplicated edges explicitly too
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 2; j++)
+      conn[i] = vtx_handles[connect[48+j]];
+    result = gMB->create_element(MBEDGE, conn, 2, elems[9+i]);
+    if (MB_SUCCESS != result) return MB_FAILURE;
+  }
+
+    // now set adjacencies explicitly
+    // front/rear duplicated edge to front/rear pair of quads
+  result = gMB->add_adjacencies(elems[9], &elems[5], 2, false);
+  if (MB_SUCCESS != result) return MB_FAILURE;
+  result = gMB->add_adjacencies(elems[10], &elems[7], 2, false);
+  if (MB_SUCCESS != result) return MB_FAILURE;
+
+    // front pair of quads to front degen hex
+  result = gMB->add_adjacencies(elems[5], &elems[4], 1, false);
+  if (MB_SUCCESS != result) return MB_FAILURE;
+  result = gMB->add_adjacencies(elems[6], &elems[4], 1, false);
+  if (MB_SUCCESS != result) return MB_FAILURE;
+
+    // rear pair of quads to rear degen hex
+  result = gMB->add_adjacencies(elems[7], &elems[5], 1, false);
+  if (MB_SUCCESS != result) return MB_FAILURE;
+  result = gMB->add_adjacencies(elems[8], &elems[5], 1, false);
+  if (MB_SUCCESS != result) return MB_FAILURE;
+
+  return MB_SUCCESS;
+}
+
+MBErrorCode make_triple_chord_push() 
+{
+  return MB_FAILURE;
+}
+
+MBErrorCode make_triple_hex_push() 
+{
+  return MB_FAILURE;
+}
+
