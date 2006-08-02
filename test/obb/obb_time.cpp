ec9cce4dc0bc684521c8069dfb041dcc407fea99
Jason Kraftcheck <kraftcheck@gmail.com>
2006-08-02 22:29:37 +0000

    Move obb_test to separate subdirectory.
    Add obb_time utility for timing ray fire at obb tree.
    
    
    git-svn-id: https://svn.mcs.anl.gov/repos/ITAPS/MOAB/trunk@797 6162379e-bd28-0410-9a7d-b7f4dcfcad3c

---
+++ test/obb/obb_time.cpp
@@ -0,0 +1,200 @@
+#include "MBCore.hpp"
+#include "MBCartVect.hpp"
+#include "MBOrientedBox.hpp"
+#include "MBOrientedBoxTreeTool.hpp"
+#include <stdlib.h>
+#include <iostream>
+#include <cmath>
+#include <time.h>
+
+const int NUM_RAYS = 20000;
+const int NUM_XSCT = 10000;
+
+static void usage( )
+{
+  std::cerr << "obb_time [-r <int>] [-i <int>] <filename>" << std::endl
+      << "  -r - Specify total rays to fire." << std::endl
+      << "       Zero implies unbounded. Default: " << NUM_RAYS << std::endl
+      << "  -i - Specify total intersecting rays to fire." << std::endl
+      << "       Zero implies unbounded. Default: " << NUM_XSCT << std::endl
+      << "  The input file should be generated using the '-s'" << std::endl
+      << "  option with 'obb_test'" << std::endl;
+  exit(1);
+}
+
+void generate_ray( const MBCartVect& sphere_center,
+                   double sphere_radius,
+                   MBCartVect& point,
+                   MBCartVect& dir )
+{
+  const int H = RAND_MAX/2;
+  point[0] = (double)rand()/H - 1;
+  point[1] = (double)rand()/H - 1;
+  point[2] = std::sqrt( 1 - point[0] * point[0] - point[1] * point[1] );
+  point *= sphere_radius;
+  point += sphere_center;
+  
+  dir[0] = (double)rand()/H - 1;
+  dir[1] = (double)rand()/H - 1;
+  dir[2] = std::sqrt( 1 - dir[0] * dir[0] - dir[1] * dir[1] );
+}
+
+MBErrorCode read_tree( MBInterface* instance,
+                       const char* filename,
+                       MBEntityHandle& tree_root_out )
+{
+  MBErrorCode rval = instance->load_mesh( filename );
+  if (MB_SUCCESS != rval)
+    return rval;
+  
+  MBTag tag;
+  rval = instance->tag_get_handle( "OBB_ROOT", tag );
+  if (MB_SUCCESS != rval)
+    return rval;
+  
+  int size;
+  MBDataType type;
+  rval = instance->tag_get_size( tag, size );
+  if (MB_SUCCESS != rval)
+    return rval;
+  rval = instance->tag_get_data_type( tag, type );
+  if (MB_SUCCESS != rval)
+    return rval;
+
+  if (size != sizeof(MBEntityHandle) || type != MB_TYPE_HANDLE)
+    return MB_FAILURE;
+  
+  return instance->tag_get_data( tag, 0, 0, &tree_root_out );
+}
+
+int main( int argc, char* argv[] )
+{
+  int num_rays = NUM_RAYS;
+  int num_xsct = NUM_XSCT;
+  const char* filename = 0;
+  
+  for (int i = 1; i < argc; ++i)
+  {
+    if (!strcmp( argv[i], "-r")) {
+      ++i;
+      if (i == argc || !argv[i][0]) {
+        std::cerr << "Expected value following '-r'" << std::endl;
+        usage();
+      }
+      char* end;
+      long t = strtol( argv[i], &end, 0 );
+      num_rays = (int)t;
+      if (*end || t < 0 || num_rays != t) {
+        std::cerr << "Expected positive integer following '-r'" << std::endl;
+        usage();
+      }
+    }
+    else if (!strcmp( argv[i], "-i")) {
+      ++i;
+      if (i == argc || !argv[i][0]) {
+        std::cerr << "Expected value following '-i'" << std::endl;
+        usage();
+      }
+      char* end;
+      long t = strtol( argv[i], &end, 0 );
+      num_xsct = (int)t;
+      if (*end || t < 0 || num_xsct != t) {
+        std::cerr << "Expected positive integer following '-i'" << std::endl;
+        usage();
+      }
+    }
+    else if (filename) {
+      std::cerr << "Invalid options or multiple file names specified." << std::endl;
+        usage();
+    }
+    else {
+      filename = argv[i];
+    }
+  }
+  if (!filename) {
+    std::cerr << "No file name specified." << std::endl;
+    usage();
+  }
+    
+  MBCore instance;
+  MBInterface* iface = &instance;
+  MBEntityHandle root;
+  MBErrorCode rval = read_tree( iface, filename, root );
+  if (MB_SUCCESS != rval) {
+    std::cerr << "Failed to read \"" <<filename<<'"'<<std::endl;
+    return 2;
+  }
+  
+  MBOrientedBoxTreeTool tool(iface);
+  MBOrientedBox box;
+  rval = tool.box( root, box );
+  if (MB_SUCCESS != rval) {
+    std::cerr << "Corrupt tree.  Cannot get box for root node." << std::endl;
+    return 3;
+  }
+  
+  const unsigned cached = 1000;
+  std::vector<double> intersections;
+  MBCartVect point, dir;
+  std::vector<MBCartVect> randrays;
+  randrays.reserve( cached );
+  int cached_idx = 0;
+  
+  clock_t t = clock();
+  int rays = 0, xsct = 0, gen = 0;
+  for (;;) {
+    if (!num_rays) {
+      if (xsct >= num_xsct)
+        break;
+    }
+    else if (!num_xsct) {
+      if (rays >= num_rays)
+        break;
+    }
+    else if (rays >= num_rays && xsct >= num_xsct)
+      break;
+    
+    ++rays;
+    MBCartVect point, dir;
+    if (randrays.size() < cached) {
+      generate_ray( box.center, box.outer_radius(), point, dir );
+      ++gen;
+    }
+    else {
+      point = randrays[cached_idx++];
+      dir   = randrays[cached_idx++];
+      cached_idx = cached_idx % randrays.size();
+    }
+    
+    intersections.clear();
+    rval = tool.ray_intersect_triangles( intersections, root, 1e-6, point.array(), dir.array() );
+    if (MB_SUCCESS != rval) {
+      std::cerr << "Rayfire #" << rays << " failed." << std::endl;
+      return 4;
+    }
+    
+    if (!intersections.empty()) {
+      ++xsct;
+    }
+    
+    if (randrays.size() < cached && 
+        (!intersections.empty() || !num_xsct || xsct >= num_xsct)) {
+      randrays.push_back( point );
+      randrays.push_back( dir );
+    }
+  }
+
+  t = clock() - t;
+  std::cout << rays << " ray fires done" << std::endl
+            << gen  << " unique rays used" << std::endl
+            << xsct << " intersecting fires" << std::endl
+            << (double)t/CLOCKS_PER_SEC << " seconds" << std::endl;
+  
+  return 0;
+}
+
+
+
+
+  
+
