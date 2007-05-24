43b8ce45c5c9e82de031b54ef8f6f63ef9aef35a
Tim Tautges <tautges@mcs.anl.gov>
2007-05-24 21:42:09 +0000

    (doing the commit right this time...)
    Going to the "right" way to do configure w/ FC_WRAPPERS.
    
    Also moved a few things around in Fortran includes for iMesh, so now
    you only need to include iMesh_f.h and iBase_f.h in the Fortran.
    
    
    git-svn-id: https://svn.mcs.anl.gov/repos/ITAPS/MOAB/trunk@1021 6162379e-bd28-0410-9a7d-b7f4dcfcad3c

--- tools/iMesh/iMesh_f.h
+++ tools/iMesh/iMesh_f.h
@@ -1,7 +1,26 @@
-
 #define iMesh_Instance integer
 #define iMesh_EntityIterator integer
 #define iMesh_EntityArrIterator integer
 #define iBase_EntityHandle integer
 #define iBase_EntitySetHandle integer
 #define iBase_TagHandle integer
+
+
+
+      parameter (iMesh_POINT = 0)
+      parameter (iMesh_LINE_SEGMENT = 1)
+      parameter (iMesh_POLYGON = 2)
+      parameter (iMesh_TRIANGLE = 3)
+      parameter (iMesh_QUADRILATERAL = 4)
+      parameter (iMesh_POLYHEDRON = 5)
+      parameter (iMesh_TETRAHEDRON = 6)
+      parameter (iMesh_HEXAHEDRON = 7)
+      parameter (iMesh_PRISM = 8)
+      parameter (iMesh_PYRAMID = 9)
+      parameter (iMesh_SEPTAHEDRON = 10)
+      parameter (iMesh_ALL_TOPOLOGIES = 11)
+
+      parameter (iMesh_UNAVAILABLE = 0)
+      parameter (iMesh_IMMEDIATE = 1)
+      parameter (iMesh_LOCAL_TRAVERSAL = 2)
+      parameter (iMesh_GLOBAL_TRAVERSAL = 3)
