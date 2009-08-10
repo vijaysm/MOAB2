2f86a254a907f6abfa609c03847c3879266a289c
Jim Porter <fathom@mcs.anl.gov>
2009-08-10 22:05:14 +0000

    Fix bad commit
    git-svn-id: https://svn.mcs.anl.gov/repos/ITAPS/MOAB/trunk@3104 6162379e-bd28-0410-9a7d-b7f4dcfcad3c

--- mhdf/src/tags.c
+++ mhdf/src/tags.c
@@ -743,6 +743,7 @@ static int get_attrib_array_length_handle( hid_t attrib_id )
   hid_t type_id;
   int rank;
   hsize_t dims[H5S_MAX_RANK];
+  int perm[H5S_MAX_RANK];
   
   type_id = H5Aget_type( attrib_id );
   switch (H5Tget_class(type_id)) {
@@ -754,6 +755,7 @@ static int get_attrib_array_length_handle( hid_t attrib_id )
       break;
     case H5T_ARRAY:
 #if defined(H5Tget_array_dims_vers) && H5Tget_array_dims_vers > 1
+      (void)perm; // eliminate warning
       rank = H5Tget_array_dims2( type_id, dims );
 #else
       rank = H5Tget_array_dims( type_id, dims, perm );
@@ -820,6 +822,7 @@ mhdf_getTagInfo( mhdf_FileHandle file_handle,
   unsigned int index;
   int rank, var_data;
   hsize_t dims[H5S_MAX_RANK];
+  int perm[H5S_MAX_RANK];
   H5T_class_t class_tmp;
 
   API_BEGIN;
@@ -986,6 +989,7 @@ mhdf_getTagInfo( mhdf_FileHandle file_handle,
     case H5T_ARRAY:
       
 #if defined(H5Tget_array_dims_vers) && H5Tget_array_dims_vers > 1
+      (void)perm; // eliminate warning
       rank = H5Tget_array_dims2( type_id, dims );
 #else
       rank = H5Tget_array_dims( type_id, dims, perm );
