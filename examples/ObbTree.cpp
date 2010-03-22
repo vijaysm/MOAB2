6cc75917ac4289db55a2bd99b6ec3278cb9c4c6e
Hong-Jun Kim <fathom@mcs.anl.gov>
2010-03-22 23:42:20 +0000

    o added a simple obb tree ray tracing example
    git-svn-id: https://svn.mcs.anl.gov/repos/ITAPS/MOAB/trunk@3693 6162379e-bd28-0410-9a7d-b7f4dcfcad3c

---
+++ examples/ObbTree.cpp
@@ -0,0 +1,74 @@
+// simple example construct obb tree and ray-tracing the tree
+// it reads triangle mesh, construct obb tree and get intersection distances
+
+#include "moab/Core.hpp"
+#include "moab/Range.hpp"
+#include "moab/OrientedBoxTreeTool.hpp"
+#include <iostream>
+#include <math.h>
+
+int main(int argc, char **argv) {
+  if (1 == argc) {
+    std::cout << "Usage: " << argv[0] << " <filename>" << std::endl;
+    return 0;
+  }
+
+  // instantiate & load a mesh from a file
+  moab::Core *mb = new moab::Core();
+  moab::ErrorCode rval = mb->load_mesh(argv[1]);
+
+  // get all triangles
+  int err;
+  moab::EntityHandle tree_root;
+  moab::Range tris;
+  //moab::OrientedBoxTreeTool::Settings settings;
+
+  rval = mb->get_entities_by_type( 0, moab::MBTRI, tris );
+  if (rval != moab::MB_SUCCESS) {
+    std::cerr << "Couldn't get triangles." << std::endl;
+    return 1;
+  }
+
+  // build OBB trees for all triangles
+  moab::OrientedBoxTreeTool tool(mb);
+  //rval = tool.build(tris, tree_root, &settings);
+  rval = tool.build(tris, tree_root);
+  if (rval != moab::MB_SUCCESS) {
+    std::cerr << "Could'nt build tree." << std::endl;    
+    return 1;
+  }
+  
+  // build box
+  double box_center[3], box_axis1[3], box_axis2[3], box_axis3[3], start_pnt[3],
+    ray_length;
+  rval = tool.box(tree_root, box_center, box_axis1, box_axis2, box_axis3);
+  if (rval != moab::MB_SUCCESS) {
+    std::cerr << "Couldn't get box for tree root set.";
+    return 1;
+  }
+
+  ray_length = 2.*sqrt(box_axis1[0]*box_axis1[0] + box_axis1[1]*box_axis1[1] +
+		       box_axis1[2]*box_axis1[2]);
+  
+  // do ray-tracing from box center to x direction
+  std::vector<double> intersections;
+  double dir[3] = {1., 0., 0.};
+  rval = tool.ray_intersect_triangles(intersections, tree_root,
+				      10e-12, box_center, dir,
+				      &ray_length);
+  if (rval != moab::MB_SUCCESS) {
+    std::cerr << "Couldn't ray tracing.";
+    return 1;
+  }
+  
+  std::cout << "ray start point: " << start_pnt[0] << " "
+	    << start_pnt[1] << " " << start_pnt[2] << std::endl;
+  std::cout << "# of intersections : " << intersections.size() << std::endl;
+  std::cout << "intersection distances are on";
+  for (int i = 0; i < intersections.size(); i++) {
+    std::cout << " " << intersections[i];
+  }
+  std::cout << " of ray length " << ray_length << std::endl;
+
+  return 0;
+}
