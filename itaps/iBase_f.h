df8f8ed8ce0b3da2b7f0dd61dabf894ce740aace
Jim Porter <fathom@mcs.anl.gov>
2011-04-21 20:24:37 +0000

    Fix fortran iBase header
    git-svn-id: https://svn.mcs.anl.gov/repos/ITAPS/MOAB/trunk@4771 6162379e-bd28-0410-9a7d-b7f4dcfcad3c

--- itaps/iBase_f.h
+++ itaps/iBase_f.h
@@ -43,7 +43,6 @@
 
       integer iBase_SUCCESS
       integer iBase_MESH_ALREADY_LOADED
-      integer iBase_NO_MESH_DATA
       integer iBase_FILE_NOT_FOUND
       integer iBase_FILE_WRITE_ERROR
       integer iBase_NIL_ARRAY
@@ -68,28 +67,27 @@
 
       parameter (iBase_SUCCESS = 0)
       parameter (iBase_MESH_ALREADY_LOADED = 1)
-      parameter (iBase_NO_MESH_DATA = 2)
-      parameter (iBase_FILE_NOT_FOUND = 3)
-      parameter (iBase_FILE_WRITE_ERROR = 4)
-      parameter (iBase_NIL_ARRAY = 5)
-      parameter (iBase_BAD_ARRAY_SIZE = 6)
-      parameter (iBase_BAD_ARRAY_DIMENSION = 7)
-      parameter (iBase_INVALID_ENTITY_HANDLE = 8)
-      parameter (iBase_INVALID_ENTITY_COUNT = 9)
-      parameter (iBase_INVALID_ENTITY_TYPE = 10)
-      parameter (iBase_INVALID_ENTITY_TOPOLOGY = 11)
-      parameter (iBase_BAD_TYPE_AND_TOPO = 12)
-      parameter (iBase_ENTITY_CREATION_ERROR = 13)
-      parameter (iBase_INVALID_TAG_HANDLE = 14)
-      parameter (iBase_TAG_NOT_FOUND = 15)
-      parameter (iBase_TAG_ALREADY_EXISTS = 16)
-      parameter (iBase_TAG_IN_USE = 17)
-      parameter (iBase_INVALID_ENTITYSET_HANDLE = 18)
-      parameter (iBase_INVALID_ITERATOR_HANDLE = 19)
-      parameter (iBase_INVALID_ARGUMENT = 20)
-      parameter (iBase_MEMORY_ALLOCATION_FAILED = 21)
-      parameter (iBase_NOT_SUPPORTED = 22)
-      parameter (iBase_FAILURE = 23)
+      parameter (iBase_FILE_NOT_FOUND = 2)
+      parameter (iBase_FILE_WRITE_ERROR = 3)
+      parameter (iBase_NIL_ARRAY = 4)
+      parameter (iBase_BAD_ARRAY_SIZE = 5)
+      parameter (iBase_BAD_ARRAY_DIMENSION = 6)
+      parameter (iBase_INVALID_ENTITY_HANDLE = 7)
+      parameter (iBase_INVALID_ENTITY_COUNT = 8)
+      parameter (iBase_INVALID_ENTITY_TYPE = 9)
+      parameter (iBase_INVALID_ENTITY_TOPOLOGY = 10)
+      parameter (iBase_BAD_TYPE_AND_TOPO = 11)
+      parameter (iBase_ENTITY_CREATION_ERROR = 12)
+      parameter (iBase_INVALID_TAG_HANDLE = 13)
+      parameter (iBase_TAG_NOT_FOUND = 14)
+      parameter (iBase_TAG_ALREADY_EXISTS = 15)
+      parameter (iBase_TAG_IN_USE = 16)
+      parameter (iBase_INVALID_ENTITYSET_HANDLE = 17)
+      parameter (iBase_INVALID_ITERATOR_HANDLE = 18)
+      parameter (iBase_INVALID_ARGUMENT = 19)
+      parameter (iBase_MEMORY_ALLOCATION_FAILED = 20)
+      parameter (iBase_NOT_SUPPORTED = 21)
+      parameter (iBase_FAILURE = 22)
 
 
       integer iBase_BLOCKED
