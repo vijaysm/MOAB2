ea258509f4f18683244bad615b0d3a45048da478
Tim Tautges <tautges@mcs.anl.gov>
2012-01-15 20:39:53 +0000

    src/io/ReadHDF5.cpp: fixing a compiler warning, and fixing a really obscure bug intersecting a file_id list   with a RangeMap.
    examples/SetsNTags.cpp: added code to list all sets, and code to run in parallel.
    
    Passes make check.
    
    
    
    git-svn-id: https://svn.mcs.anl.gov/repos/ITAPS/MOAB/trunk@5321 6162379e-bd28-0410-9a7d-b7f4dcfcad3c

--- examples/SetsNTags.cpp
+++ examples/SetsNTags.cpp
@@ -4,21 +4,27 @@
 #include <iostream>
 
 int main(int argc, char **argv) {
-  if (1 == argc) {
-    std::cout << "Usage: " << argv[0] << " <filename>" << std::endl;
-    return 0;
-  }
-
     // get the material set tag handle
   moab::Tag mtag;
   moab::ErrorCode rval;
-  const char *tag_nms[] = {"MATERIAL_SET", "DIRICHLET_SET", 
-                           "NEUMANN_SET"};
+  const char *tag_nms[] = {"MATERIAL_SET", "DIRICHLET_SET", "NEUMANN_SET"};
   moab::Range sets, set_ents;
 
     // instantiate & load a file
   moab::Interface *mb = new moab::Core();
-  rval = mb->load_file(argv[1]);
+  const char *par_opt = "PARALLEL=READ_PART;PARTITION=PARALLEL_PARTITION;PARTITION_DISTRIBUTE;PARALLEL_RESOLVE_SHARED_ENTS;SETS=SETS";
+
+  bool parallel = false;
+  if (argc > 2 && !strcmp(argv[1], "-p")) parallel = true;
+  else if (argc == 1) {
+    std::cout << "Usage: " << argv[0] << "[-p] <filename>" << std::endl;
+    return 0;
+  }
+  
+  if (parallel) 
+    rval = mb->load_file(argv[argc-1], 0, par_opt);
+  else
+    rval = mb->load_file(argv[argc-1]);
 
     // loop over set types
   for (int i = 0; i < 3; i++) {
@@ -42,8 +48,18 @@ int main(int argc, char **argv) {
       rval = mb->get_entities_by_handle(this_set, set_ents, true);
 
       std::cout << tag_nms[i] << " " << set_id << " has " 
-                << set_ents.size() << " entities." << std::endl;
+                << set_ents.size() << " entities:" << std::endl;
+      set_ents.print("   ");
       set_ents.clear();
     }
   }
+
+    // do the same for all sets
+  sets.clear();
+  rval = mb->get_entities_by_type(0, moab::MBENTITYSET, sets);
+
+    // print the sets
+  rval = mb->list_entities(sets);
+
+  delete mb;
 }
