64c0272b85c5cd2d90169da125c7adb2e331ae1f
Steve Jackson <fathom@mcs.anl.gov>
2012-06-22 22:27:08 +0000

    Give dagmc_preproc a feature to update mcnp5 property syntax
    The syntax of the 'tally' and 'imp' metadata group names is
    changing to become compatible with the new property system.
    This optional argument updates from the old dag-mcnp5 naming
    convention to the new one.
    
    This may not be the final home of this feature.
    
    git-svn-id: https://svn.mcs.anl.gov/repos/ITAPS/MOAB/trunk@5588 6162379e-bd28-0410-9a7d-b7f4dcfcad3c

--- tools/dagmc/dagmc_preproc.cpp
+++ tools/dagmc/dagmc_preproc.cpp
@@ -11,6 +11,7 @@
 #include <iomanip>
 #include <cstdlib>
 #include <algorithm>
+#include <cstdio>
 
 using namespace moab;
 
@@ -37,6 +38,64 @@ void chkerr( Interface& mbi, ErrorCode code, int line, const char* file ){
 
 void chkerr( DagMC& dag, ErrorCode code, int line, const char* file ){
   chkerr( dag.moab_instance(), code, line, file );
+}
+
+
+ErrorCode update_mcnp5_property_names( Interface* MBI )
+{
+  ErrorCode rval;
+  Tag category_tag;
+  rval = MBI->tag_get_handle( CATEGORY_TAG_NAME, 32, MB_TYPE_OPAQUE, category_tag );
+  if (MB_SUCCESS != rval)
+    return rval;
+  char group_category[CATEGORY_TAG_SIZE];
+  std::fill(group_category, group_category+CATEGORY_TAG_SIZE, '\0');
+  sprintf(group_category, "%s", "Group");
+  const void* const group_val[] = {&group_category};
+  Range groups;
+  rval = MBI->get_entities_by_type_and_tag(0, MBENTITYSET, &category_tag, 
+                                           group_val, 1, groups);
+  if (MB_SUCCESS != rval)
+    return rval;
+
+  Tag name_tag;
+  rval = MBI->tag_get_handle( NAME_TAG_NAME, NAME_TAG_SIZE, MB_TYPE_OPAQUE, name_tag );
+  if (MB_SUCCESS != rval)
+    return rval;
+  for( Range::iterator i = groups.begin(); i != groups.end(); ++i ){
+    EntityHandle grp = *i;
+    const void* p;
+    int ignored;
+    rval = MBI->tag_get_by_ptr( name_tag, &grp, 1, &p, &ignored );
+    if( MB_SUCCESS != rval ) return rval;
+    const char* grpname = static_cast<const char*>(p);
+    std::string modname(grpname);
+    size_t idx;
+    if( modname.find("tally_") == 0 ){
+        std::string arg = modname.substr(6);
+        // replace up to 2 underscores
+        int count = 0;
+        while( count < 2 &&  (idx = arg.find_first_of("_")) != arg.npos )
+        {
+          count ++;
+          arg[idx] = '.';
+        }
+        modname = modname.substr(0,6) + arg;
+    }
+    else if( (idx = modname.find("imp_")) != modname.npos ){
+        modname[idx+3] = ':';
+    }
+
+    if( modname != grpname ){
+      std::cout << "Group name " << grpname << " changed to " << modname << std::endl;
+      p = static_cast<const void*>(modname.c_str());
+      int length = NAME_TAG_SIZE;
+      rval = MBI->tag_set_by_ptr( name_tag, &grp, 1, &p, &length);
+      if( MB_SUCCESS != rval ) return rval;
+    }
+  }
+
+  return MB_SUCCESS;
 }
 
 /**
@@ -248,6 +307,7 @@ int main( int argc, char* argv[] ){
   po.addOpt<int>( "obb-vis-divs", "Resolution of obb visualization grid (default 50)", &grid );
   po.addOpt<void>( "obb-stats,S", "Print obb statistics.  With -v, print verbose statistics." );
   po.addOpt<std::vector<int> >( "vols,V", "Specify a set of volumes (applies to --obb_vis and --obb_stats, default all)" );
+  po.addOpt<void>( "mcnp5-props", "Update MCNP5 property names" );
   po.addOptionHelpHeading("Options for loading CAD files");
   po.addOpt<double>( "ftol,f", "Faceting distance tolerance", po.add_cancel_opt );
   po.addOpt<double>( "ltol,l", "Faceting edge length tolerance", po.add_cancel_opt );
@@ -325,6 +385,11 @@ int main( int argc, char* argv[] ){
     CHECKERR( mbi, ret );
   }
 
+  if( po.numOptSet( "mcnp5-props" ) ){
+    ret = update_mcnp5_property_names( &mbi );
+    CHECKERR( mbi, ret );
+  }
+
   /* Iterate through any -m alternate mesh files and replace surfaces */
 
   std::vector<std::string> m_list;
@@ -343,7 +408,7 @@ int main( int argc, char* argv[] ){
     CHECKERR( mbi, ret );
 
     ret = mbi.tag_get_handle(GLOBAL_ID_TAG_NAME, 1, MB_TYPE_INTEGER, 
-			     idTag, MB_TAG_DENSE|MB_TAG_CREAT);
+                             idTag, MB_TAG_DENSE|MB_TAG_CREAT);
     CHECKERR( mbi, ret );
 
     ret = mbi.tag_get_handle("GEOM_SENSE_2", 2, MB_TYPE_HANDLE, senseTag, 
