7c8f19c95bd32a354c998fc5410f0069bc74a0aa
Ryan Lewis <fathom@mcs.anl.gov>
2012-08-03 18:37:15 +0000

    level 2
    git-svn-id: https://svn.mcs.anl.gov/repos/ITAPS/MOAB/trunk@5657 6162379e-bd28-0410-9a7d-b7f4dcfcad3c

--- src/moab/point_locater/point_locater.hpp
+++ src/moab/point_locater/point_locater.hpp
@@ -18,6 +18,8 @@ class Point_search {
 public:
 	typedef  _Tree Tree;
 	typedef typename Elements::value_type Element;
+	//arbitrary type for now.. A box should be a small struct, boxes a
+	//Vector of them, or a set?
 	typedef	std::vector< std::vector< std::size_t> > Boxes;
 	//temporary error code
 	typedef typename std::size_t Error;
@@ -37,19 +39,29 @@ Point_search( Elements & _source_elements,
 	      source_elements( _source_elements),
 	      comm( _comm), boxes(), 
 	      iterations( _iterations){}
+//private functionality
+private:
+template< typename Points, typename Bitmask>
+void local_point_search( const Points & query_points, Bitmask & located_points, 
+			 Elements & result,
+			 const double rel_eps, const double abs_eps){}	
 
+//public functionality
+public:
 template< typename Points> 
 Error locate_points( Points & query_points, Elements & result,
                      const double rel_eps, const double abs_eps){
+	//bitmask for located points
+	std::vector< bool> located_points( query_points.size(), 0);
+	local_point_search( query_points, located_points, 
+			    result, rel_eps, abs_eps);
+	resolve_boxes( query_points, located_points, rel_eps, abs_eps);
+	//scatter-gather
+	//TODO: probably too good to be true..
+	local_point_search( query_points, located_points, 
+			    result, rel_eps, abs_eps);
+	//send back to target
 	return 0;
-} 
-
-template< typename Points> 
-Error locate_points( Points & query_points,
-                     const double rel_eps = 0.0, 
-                     const double abs_eps = 0.0){
-     Elements result;
-     return locate_points( query_points, result, rel_eps, abs_eps);
 } 
 
 //public accessor methods
