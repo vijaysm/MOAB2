48a3910233a8bd0567c18b68e1cfdead67363a5b
Vijay S. Mahadevan <vijay.m@gmail.com>
2013-01-23 00:46:39 +0000

    Reverting the virtual destructor addition to CartVect. This was needed since BSPTreePoly::Vertex derives from this structure but since CartVect only contains POD types, there is no value in increasing the memory footprint for the sake of derivation.
    make check passes all tests now while previously with the virtual destructor both tools/refiner/test_mesh_refiner 
    and tools/mbcoupler_test failed in debug mode.
    
    
    
    git-svn-id: https://svn.mcs.anl.gov/repos/ITAPS/MOAB/trunk@5953 6162379e-bd28-0410-9a7d-b7f4dcfcad3c

--- src/moab/CartVect.hpp
+++ src/moab/CartVect.hpp
@@ -1,16 +1,16 @@
 /**
  * MOAB, a Mesh-Oriented datABase, is a software component for creating,
  * storing and accessing finite element mesh data.
- * 
+ *
  * Copyright 2004 Sandia Corporation.  Under the terms of Contract
  * DE-AC04-94AL85000 with Sandia Coroporation, the U.S. Government
  * retains certain rights in this software.
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  */
 
 #ifndef MB_CART_VECT_HPP
@@ -31,13 +31,13 @@ class CartVect
 {
   private:
     double d[3];
-  
+
   public:
-    
-    inline CartVect() 
+
+    inline CartVect()
       { }
       /**Initialze all three values to same scalar (typically zero)*/
-    explicit inline CartVect( double v ) 
+    explicit inline CartVect( double v )
       { d[0] = d[1] = d[2] = v; }
     inline CartVect( double i, double j, double k )
       { d[0] = i; d[1] = j; d[2] = k; }
@@ -47,14 +47,11 @@ class CartVect
     inline CartVect& operator=( const double v[3] )
       { d[0]= v[0]; d[1] = v[1]; d[2] = v[2]; return *this; }
 
-    virtual ~CartVect()
-      { }
-
     inline double& operator[]( unsigned i )
       { return d[i]; }
     inline double operator[]( unsigned i ) const
       { return d[i]; }
-    
+
     inline CartVect& operator+=( const CartVect& v )
       { d[0] += v.d[0]; d[1] += v.d[1]; d[2] += v.d[2]; return *this; }
     inline CartVect& operator-=( const CartVect& v )
@@ -70,25 +67,25 @@ class CartVect
       { d[0] *= s; d[1] *= s; d[2] *= s; return *this; }
     inline CartVect& operator/=( double s )
       { d[0] /= s; d[1] /= s; d[2] /= s; return *this; }
-    
+
     inline double length() const; //!< vector length
-    
+
     inline double length_squared() const;
-    
+
     inline void normalize(); //!< make unit length
-    
+
     inline void flip(); //!< flip direction
-    
+
       /** per-element scalar multiply (this[0] *= v[0], this[1] *= v[1], ...) */
     inline void scale( const CartVect& v )
       { d[0] *= v.d[0]; d[1] *= v.d[1]; d[2] *= v.d[2]; }
-    
+
       // get pointer to array of three doubles
     inline double* array()
       { return d; }
     inline const double* array() const
       { return d; }
-      
+
       /** initialize array from this */
     inline void get( double v[3] ) const
       { v[0] = d[0]; v[1] = d[1]; v[2] = d[2]; }
@@ -133,7 +130,7 @@ inline double angle( const CartVect& u, const CartVect& v )
 
 inline CartVect operator-( const CartVect& v )
   { return CartVect( -v[0], -v[1], -v[2] ); }
-inline CartVect operator+( const CartVect& v, double s ) 
+inline CartVect operator+( const CartVect& v, double s )
   { return CartVect( v[0] + s, v[1] + s, v[2] + s ); }
 inline CartVect operator-( const CartVect& v, double s )
   { return CartVect( v[0] - s, v[1] - s, v[2] - s ); }
@@ -148,14 +145,14 @@ inline CartVect operator-( double s, const CartVect& v )
 inline CartVect operator*( double s, const CartVect& v )
   { return CartVect( v[0] * s, v[1] * s, v[2] * s ); }
 
-//! Get unit vector in same direction 
+//! Get unit vector in same direction
 inline CartVect unit( const CartVect& v )
 {
   const double len = v.length();
-  return CartVect( v[0] / len, v[1] / len, v[2] / len ); 
+  return CartVect( v[0] / len, v[1] / len, v[2] / len );
 }
 
-    
+
 std::ostream& operator<<( std::ostream& s, const CartVect& v );
 
 } // namespace moab
