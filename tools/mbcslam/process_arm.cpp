d560d37adf9eb8e664d416027ee7c950f650a314
Iulian Grindeanu <iulian@mcs.anl.gov>
2013-04-21 23:07:39 -0500

    finish postprocessing files from mark correct the connectivity; use connectivity created using spectral visu from the initial ARM file; then match the points to the points created from data file; plot and animate velocity

--- tools/mbcslam/process_arm.cpp
+++ tools/mbcslam/process_arm.cpp
@@ -17,6 +17,7 @@
 #include "moab/Core.hpp"
 #include "moab/Interface.hpp"
 #include "ReadUtilIface.hpp"
+#include "AdaptiveKDTree.hpp"
 
 #include "CslamUtils.hpp"
 
@@ -88,16 +89,44 @@ using namespace moab;
       std::cout<<"ReadNCDF:: Problem getting variable "<< name<<"\n";\
       return 1;}}}
 
+/*
+int get_2d_flt_var(int ncFile, const char * name, int index, std::vector<float> & vals)
+{
+  int id;
+  std::vector<int> dum_dims;
+  GET_VAR(name, id, dum_dims);
+  if (-1 != id) {
+    size_t ntmp;
+  int dvfail = nc_inq_dimlen(ncFile, dum_dims[1], &ntmp);
+  vals.resize(ntmp);
+  size_t ntmp1[2] = {index, 0};
+
+  int ntimes;
+  dvfail = nc_inq_dimlen(ncFile, dum_dims[0], &ntimes);
+  if (index>=ntimes) {
+    std::cout<<"ReadNCDF:: Problem getting variable "<< name<<"\n";
+    return 1;
+  }
+  size_t count[2] ={1, ntmp};
+  dvfail = nc_get_vara_float(ncFile, id, ntmp1, count, &vals[0]);
+  if (NC_NOERR != dvfail) {
+    std::cout<<"ReadNCDF:: Problem getting variable "<< name<<"\n";
+    return 1;
+  }
+  return 0;
+}
+*/
 /* get the variable along an index */
 #define GET_2D_FLT_VAR(name, id, index, vals) \
     {std::vector<int> dum_dims;        \
   GET_VAR(name, id, dum_dims);\
   if (-1 != id) {\
-    size_t ntmp;\
+    size_t ntmp, ntmp2;\
     int dvfail = nc_inq_dimlen(ncFile, dum_dims[1], &ntmp);\
+    dvfail = nc_inq_dimlen(ncFile, dum_dims[0], &ntmp2);\
     vals.resize(ntmp);\
     size_t ntmp1[2] = {index, 0}; \
-    if (index>=dum_dims[0]) { \
+    if (index>=ntmp2) { \
       std::cout<<"ReadNCDF:: Problem getting variable "<< name<<"\n"; \
       return 1; \
     } \
@@ -110,8 +139,6 @@ using namespace moab;
 int main(int argc, char ** argv)
 {
 
-
-
   int num_el = 50000;
   if (argc>1)
   {
@@ -121,29 +148,11 @@ int main(int argc, char ** argv)
 
   Core moab;
   Interface & mb = moab;
-  //EntityHandle euler_set;
   ErrorCode rval;
-  /*
-  // this can be directly imported to moab
-  const char *sphere_mesh = "ARM_30_x8.g";
-  rval = mb.create_meshset(MESHSET_SET, euler_set);
-  if (MB_SUCCESS != rval)
-    return 1;
 
-  rval = mb.load_file(sphere_mesh, &euler_set);
-
-  if (MB_SUCCESS != rval)
-    return 1;
-
-  mb.list_entities(&euler_set, 1);*/
-
-  // now, open the connectivity file and read the element corners variable
   int ncFile, temp_dim;        // netcdf/exodus file
 
-  // now, load the other file
-
   // now, open the data file and read the lat, lon and U850 and V850
-
   const char *data_file = "fc5_arm12.cam2.h2.0004-12-12-00000.nc";
 
   int fail = nc_open(data_file, 0, &ncFile);
@@ -164,7 +173,6 @@ int main(int argc, char ** argv)
   std::cout<< " lat, lon 0" << lat[0] << " " << lon[0] << "\n";
   std::cout<< " lat, lon 1" << lat[1] << " " << lon[1] << "\n";
   std::cout << " size: " << lat.size() << "\n";
-
 
   // see if the mesh from metadata makes sense
   // create quads with the connectivity from conn array
@@ -206,39 +214,6 @@ int main(int argc, char ** argv)
   if (MB_SUCCESS!= rval)
        return 1;
 
-  // read the U850 and V850 variables
-  std::vector<float> u850;
-  GET_2D_FLT_VAR("U850", temp_dim, 0, u850);
-  std::vector<float> v850;
-  GET_2D_FLT_VAR("V850", temp_dim, 0, v850);
-
-  std::cout << " U850:" << u850[0] << " " << u850[1] << " " << u850[5] << " "<< u850.size()<<"\n";
-  std::cout << " V850:" << v850[0] << " " << v850[1] << " " << v850[5] << " "<< u850.size()<<"\n";
-  // ok, use radius as 6371km; not needed
-  /*
-     *  CartVect res;
-    res[0] = sc.R * cos(sc.lat)*cos(sc.lon) ; // x coordinate
-    res[1] = sc.R * cos(sc.lat)*sin(sc.lon); // y
-    res[2] = sc.R * sin(sc.lat);             // z
-    return res;
-     */
-  std::vector<CartVect> velo850(ncol);
-  Tag velotag;
-  rval = mb.tag_get_handle("VELO", 3, MB_TYPE_DOUBLE,
-        velotag, MB_TAG_DENSE|MB_TAG_CREAT);
-  if (MB_SUCCESS!= rval)
-    return 1;
-  for (int k=0; k<ncol; k++)
-  {
-    double latRad=lat[k]*conversion_factor;
-    double lonRad=lon[k]*conversion_factor;
-    CartVect U(-sin(lonRad), cos(lonRad), 0.);
-    CartVect V(-sin(latRad)*cos(lonRad), -sin(latRad)*cos(lonRad), cos(latRad));
-    velo850[k]=U*u850[k] +V*v850[k];
-  }
-  rval = mb.tag_set_data(velotag, nodes, &(velo850[0][0]));
-  if (MB_SUCCESS!= rval)
-      return 1;
   EntityHandle newSet;
   rval = mb.create_meshset(MESHSET_SET, newSet);
   if (MB_SUCCESS != rval)
@@ -247,36 +222,98 @@ int main(int argc, char ** argv)
   // so the nodes will be part
   mb.add_entities(newSet, nodes);
 
-
-
-  const char *conn_file = "ARM_30_x8-metadata.g";
-
-  fail = nc_open(conn_file, 0, &ncFile);
-  if (NC_NOWRITE != fail)
-  {
-    std::cout << "ReadNCDF:: problem opening Netcdf/Exodus II " << conn_file
-        << "\n";
+  // build a kd tree with the vertices
+  EntityHandle tree_root;
+  AdaptiveKDTree kd(&mb, true);
+  rval = kd.build_tree(nodes, tree_root);
+  if (MB_SUCCESS != rval)
     return 1;
+
+  unsigned int  min_depth, max_depth;
+  rval = kd.depth(tree_root, min_depth, max_depth);
+  if (MB_SUCCESS != rval)
+     return 1;
+  std::cout << "min_depth, max_depth " << min_depth << " " << max_depth << "\n";
+  // now, read the conn file created using spectral visu, and see how they fit
+  // this can be directly imported to moab
+  const char *myconn_file = "spec.R2.vtk";
+  EntityHandle euler_set;
+  rval = mb.create_meshset(MESHSET_SET, euler_set);
+  if (MB_SUCCESS != rval)
+    return 1;
+
+  rval = mb.load_file(myconn_file, &euler_set);
+
+  if (MB_SUCCESS != rval)
+    return 1;
+
+  mb.list_entities(&euler_set, 1);
+
+  Range specQuads;
+  rval = mb.get_entities_by_dimension(euler_set, 2, specQuads );
+  if (MB_SUCCESS != rval)
+     return 1;
+
+  Range vertices;
+  rval = mb.get_connectivity(specQuads, vertices);
+  if (MB_SUCCESS != rval)
+     return 1;
+
+  // do a mapping, from position of vertices to the vertices in the kd tree.
+  // find the closest vertex to each of this
+  std::vector<EntityHandle> mappedTo(vertices.size());
+  std::vector<double> mycoords(vertices.size()*3);
+  rval = mb.get_coords(vertices, &mycoords[0]);
+  double * ptr = &mycoords[0];
+  size_t num_verts=vertices.size();
+  for (size_t i=0; i<num_verts; i++, ptr+=3)
+  {
+    CartVect pos(ptr); // take 3 coordinates
+    std::vector<EntityHandle> leaves;
+    rval = kd.leaves_within_distance( tree_root,
+                                          ptr,
+                                          0.001,
+                                        leaves);
+    if (MB_SUCCESS != rval)
+      return 1;
+    Range closeVerts;
+    for (std::vector<EntityHandle>::iterator vit = leaves.begin(); vit != leaves.end(); vit++)
+    {
+      rval= mb.get_entities_by_handle(*vit, closeVerts, Interface::UNION);
+      if (moab::MB_SUCCESS != rval)
+        return 1;
+    }
+    if (closeVerts.size()<1)
+    {
+      std::cout << "increase tolerance, no points close to " << pos << "\n";
+      return 1;
+    }
+    std::vector<CartVect> coordsTarget(closeVerts.size());
+    rval = mb.get_coords(closeVerts, &(coordsTarget[0][0]));
+    if (MB_SUCCESS != rval)
+      return 1;
+    double minDist2=(pos-coordsTarget[0]).length_squared();
+    EntityHandle closestVertex=closeVerts[0];
+    for (unsigned int j=1; j<closeVerts.size(); j++)
+    {
+      double dist2=(pos-coordsTarget[j]).length_squared();
+      if (minDist2>dist2)
+      {
+        closestVertex = closeVerts[j];
+        minDist2=dist2;
+      }
+    }
+    if (minDist2 > 0.00001)
+    {
+      std::cout << " problem with node " << vertices[i] << "  min dist2:" << minDist2 << "\n";
+      return 1;
+    }
+    mappedTo [i] = closestVertex;
   }
 
-  // read in initial variables
-  int  ncenters, ncorners;
-  GET_DIM(temp_dim, "ncenters", ncenters);
-  GET_DIM(temp_dim, "ncorners", ncorners);
-  std::cout << " ncenters, ncorners:" << ncenters << " " << ncorners << "\n";
-
-  int nc_var;
-  //int block_seq_id = 1;
-  std::vector<int> dims;
-
-  GET_VAR("element_corners", nc_var, dims);
-  std::cout << " element corners dims:" << dims.size() << "\n";
-  for (size_t i = 0; i < dims.size(); i++)
-    std::cout << " dim " << i << " " << dims[i] << "\n";
-
+  num_el = (int)specQuads.size();
   // tmp_ptr is of type int* and points at the same place as conn
   EntityHandle * conn = 0;
-  //conn = new int[ncenters * ncorners];
 
   EntityHandle elh;
 
@@ -288,25 +325,21 @@ int main(int argc, char ** argv)
           elh,
           conn);
 
-  // read the connetivity into that memory,  this will take only part of the array
-  // 1/2 if sizeof(EntityHandle) == 64 bits.
-  size_t start[2] = { 0, 0 };
-  size_t count[2] = { ncorners, ncenters };
-
-  int* tmp_ptr =new int [ncorners*ncenters];
-  fail = nc_get_vara_int(ncFile, nc_var, start, count, tmp_ptr);
-  if (NC_NOERR != fail)
-    return 1;
-  // now move connectivity here, using start handle for new vertices
-  // also, permute connectivity; it is
+  EntityHandle firstVertMyMesh=vertices[0];
   for (int k=0; k<num_el; k++)
   {
+    const EntityHandle * myconn=0;
+    EntityHandle specElem = specQuads[k];
+    int num_nodes=0;
+    rval = mb.get_connectivity(specElem, myconn, num_nodes);
+    if (MB_SUCCESS != rval || num_nodes !=4)
+      return 1;
+
     int start_el = k*4;
     for (int j=0; j<4; j++)
-       conn[start_el+j] = node_handle + tmp_ptr[j*ncenters+k]-1;
+       conn[start_el+j] = mappedTo[myconn[j]-firstVertMyMesh];
   }
   std::cout << " conn:" << conn[0] << " " << conn[1] << " " << conn[3]<< "\n";
-
   Range erange(elh, elh+num_el-1);
 
   mb.add_entities(newSet, erange);
@@ -321,7 +354,51 @@ int main(int argc, char ** argv)
     gidels[k]=k+1;
   mb.tag_set_data(gid2, erange, &gidels[0]);
 
-  mb.write_file("tt2.h5m", 0, 0, &newSet, 1);
+  int times;
+  GET_DIM(temp_dim, "time", times);
+  Tag velotag;
+  rval = mb.tag_get_handle("VELO", 3, MB_TYPE_DOUBLE,
+            velotag, MB_TAG_DENSE|MB_TAG_CREAT);
+  if (MB_SUCCESS!= rval)
+    return 1;
+
+  for (size_t tt=0; tt<(size_t)times; tt++)
+  {
+    // now, read velocities from file:
+    // read the U850 and V850 variables
+    std::vector<float> u850;
+    GET_2D_FLT_VAR("U850", temp_dim, tt, u850);
+    std::vector<float> v850;
+    GET_2D_FLT_VAR("V850", temp_dim, tt, v850);
+
+    std::cout << " U850:" << u850[0] << " " << u850[1] << " " << u850[5] << " "<< u850.size()<<"\n";
+    std::cout << " V850:" << v850[0] << " " << v850[1] << " " << v850[5] << " "<< u850.size()<<"\n";
+    // ok, use radius as 6371km; not needed
+
+    std::vector<CartVect> velo850(ncol);
+
+    std::stringstream fileName;
+    fileName << "VELO0" <<  tt << ".vtk";
+    std::cout << " read velocities at 850 for time:" << tt << "\n";
+
+
+    for (int k=0; k<ncol; k++)
+    {
+      double latRad=lat[k]*conversion_factor;
+      double lonRad=lon[k]*conversion_factor;
+      CartVect U(-sin(lonRad), cos(lonRad), 0.);
+      CartVect V(-sin(latRad)*cos(lonRad), -sin(latRad)*cos(lonRad), cos(latRad));
+      velo850[k]=U*u850[k] +V*v850[k];
+    }
+    rval = mb.tag_set_data(velotag, nodes, &(velo850[0][0]));
+    if (MB_SUCCESS!= rval)
+      return 1;
+    rval = mb.write_mesh(fileName.str().c_str(), &newSet, 1);
+    if (MB_SUCCESS!= rval)
+      return 1;
+  }
+
+
 
   return 0;
 }
