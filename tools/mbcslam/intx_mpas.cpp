7451a26b6316b2b35d83d878f9d21e819bd88623
Iulian Grindeanu <iulian@mcs.anl.gov>
2013-06-29 12:53:03 -0500

    the mpas example can run in parallel in mbcslam folder
    it is using the 8-part partition mpas_p8.h5m file; by default it runs in
    serial, but it can be run in parallel with mpiexec -np <np> ...

--- tools/mbcslam/intx_mpas.cpp
+++ tools/mbcslam/intx_mpas.cpp
@@ -18,6 +18,8 @@
 #include <math.h>
 #include "moab/ProgOptions.hpp"
 #include "MBTagConventions.hpp"
+#include "TestUtil.hpp"
+#include "moab/ParallelComm.hpp"
 
 #include "CslamUtils.hpp"
 
@@ -42,7 +44,7 @@ double gtol = 1.e-9; // this is for geometry tolerance
 double t = 0.1, delta_t = 0.1; // check the script
 
 ErrorCode manufacture_lagrange_mesh_on_sphere(Interface * mb,
-    EntityHandle euler_set, EntityHandle & lagr_set)
+    EntityHandle euler_set)
 {
   ErrorCode rval = MB_SUCCESS;
 
@@ -62,58 +64,53 @@ ErrorCode manufacture_lagrange_mesh_on_sphere(Interface * mb,
   if (MB_SUCCESS != rval)
     return rval;
 
-  // create new set
-  rval = mb->create_meshset(MESHSET_SET, lagr_set);
-  if (MB_SUCCESS != rval)
-    return rval;
 
-  // get the coordinates of the old mesh, and move it around the sphere according to case 1
+  Tag tagh = 0;
+  std::string tag_name("DP");
+  rval = mb->tag_get_handle(tag_name.c_str(), 3, MB_TYPE_DOUBLE, tagh, MB_TAG_DENSE | MB_TAG_CREAT);
+  CHECK_ERR(rval);
+  void *data; // pointer to the LOC in memory, for each vertex
+  int count;
+
+  rval = mb->tag_iterate(tagh, connecVerts.begin(), connecVerts.end(), count, data);
+  CHECK_ERR(rval);
+  // here we are checking contiguity
+  assert(count == (int) connecVerts.size());
+  double * ptr_DP=(double*)data;
+  // get the coordinates of the old mesh, and move it around the sphere in the same way as in the
+  // python script
+
   // now put the vertices in the right place....
   //int vix=0; // vertex index in new array
-
-  // first create departure points (vertices in the lagrange mesh)
-  // then connect them in quads
-  std::map<EntityHandle, EntityHandle> newNodes;
-  for (Range::iterator vit = connecVerts.begin(); vit != connecVerts.end();
-      vit++)
+  double t=0.1, T=5;// check the script
+  double time =0.05;
+  double rot= M_PI/10;
+  for (Range::iterator vit=connecVerts.begin();vit!=connecVerts.end(); vit++ )
   {
-    EntityHandle oldV = *vit;
+    EntityHandle oldV=*vit;
     CartVect posi;
-    rval = mb->get_coords(&oldV, 1, &(posi[0]));
-    if (MB_SUCCESS != rval)
-      return rval;
-    // cslam utils, case 1
-    CartVect newPos;
-    departure_point_case1(posi, t, delta_t, newPos);
-    newPos = radius * newPos;
-    EntityHandle new_vert;
-    rval = mb->create_vertex(&(newPos[0]), new_vert);
-    if (MB_SUCCESS != rval)
-      return rval;
-    newNodes[oldV] = new_vert;
-  }
-  EntityHandle new_conn[MAXEDGES]; // up to 10, for the time being
-  for (Range::iterator it = polygons.begin(); it != polygons.end(); it++)
-  {
-    EntityHandle q = *it;
-    int nnodes;
-    const EntityHandle * conn4;
-    rval = mb->get_connectivity(q, conn4, nnodes);
-    if (MB_SUCCESS != rval)
-      return rval;
+    rval = mb->get_coords(&oldV, 1, &(posi[0]) );
+    CHECK_ERR(rval);
+    // do some mumbo jumbo, as in python script
+    SphereCoords sphCoord = cart_to_spherical(posi);
+    double lat1 = sphCoord.lat-2*M_PI*t/T; // 0.1/5
+    double uu = 3*radius/ T * pow(sin(lat1), 2)*sin(2*sphCoord.lon)*cos(M_PI*t/T);
+    uu+=2*M_PI*cos(sphCoord.lon)/T;
+    double vv = 3*radius/T*(sin(2*lat1))*cos(sphCoord.lon)*cos(M_PI*t/T);
+    double vx = -uu*sin(sphCoord.lon)-vv*sin(sphCoord.lat)*cos(sphCoord.lon);
+    double vy = -uu*cos(sphCoord.lon)-vv*sin(sphCoord.lat)*sin(sphCoord.lon);
+    double vz = vv*cos(sphCoord.lat);
+    posi = posi + time * CartVect(vx, vy, vz);
+    double x2= posi[0]*cos(rot)-posi[1]*sin(rot);
+    double y2= posi[0]*sin(rot) + posi[1]*cos(rot);
+    CartVect newPos(x2, y2, posi[2]);
+    double len1= newPos.length();
+    newPos = radius*newPos/len1;
 
-    for (int i = 0; i < nnodes; i++)
-    {
-      EntityHandle v1 = conn4[i];
-      new_conn[i] = newNodes[v1];
-    }
-    EntityHandle new_poly;
-    rval = mb->create_element(MBPOLYGON, new_conn, nnodes, new_poly);
-    if (MB_SUCCESS != rval)
-      return rval;
-    rval = mb->add_entities(lagr_set, &new_poly, 1);
-    if (MB_SUCCESS != rval)
-      return rval;
+    ptr_DP[0]=newPos[0];
+    ptr_DP[1]=newPos[1];
+    ptr_DP[2]=newPos[2];
+    ptr_DP+=3; // increment to the next node
   }
 
   return rval;
@@ -170,16 +167,24 @@ int main(int argc, char **argv)
     std::cout << " case 1: use -gtol " << gtol << " -dt " << delta_t <<
         " -input " << filename_mesh1 << "\n";
 
-  // everybody will get a DP tag, including the non owned entities; so exchange tags is not required for LOC (here)
-  EntityHandle lagrange_set;
-  rval = manufacture_lagrange_mesh_on_sphere(&mb, euler_set, lagrange_set);
+  rval = manufacture_lagrange_mesh_on_sphere(&mb, euler_set);
   if (MB_SUCCESS != rval)
     return 1;
-  rval = mb.write_file("lagrIni.h5m", 0, 0, &lagrange_set, 1);
- if (MB_SUCCESS != rval)
-   std::cout << "can't write lagr set\n";
 
-  rval = enforce_convexity(&mb, lagrange_set);
+  EntityHandle covering_lagr_set;
+  rval = mb.create_meshset(MESHSET_SET, covering_lagr_set);
+  CHECK_ERR(rval);
+  Intx2MeshOnSphere worker(&mb);
+
+  double radius = 1.; // input
+
+  worker.SetRadius(radius);
+
+  worker.SetErrorTolerance(gtol);
+  rval = worker.create_departure_mesh_2nd_alg(euler_set, covering_lagr_set);
+  CHECK_ERR(rval);
+
+  rval = enforce_convexity(&mb, covering_lagr_set);
   if (MB_SUCCESS != rval)
     return 1;
 
@@ -190,25 +195,17 @@ int main(int argc, char **argv)
   if (MB_SUCCESS != rval)
     std::cout << "can't write lagr set\n";
 
-  Intx2MeshOnSphere worker(&mb);
-
-  double radius = 1.; // input
-
-  worker.SetRadius(radius);
-
-  worker.SetErrorTolerance(gtol);
-
   EntityHandle outputSet;
   rval = mb.create_meshset(MESHSET_SET, outputSet);
   if (MB_SUCCESS != rval)
     return 1;
-  rval = worker.intersect_meshes(lagrange_set, euler_set, outputSet);
+  rval = worker.intersect_meshes(covering_lagr_set, euler_set, outputSet);
   if (MB_SUCCESS != rval)
     return 1;
 
   std::string opts_write("");
   std::stringstream outf;
-  outf << "intersect1" << ".h5m";
+  outf << "intersect0" << rank << ".h5m";
   rval = mb.write_file(outf.str().c_str(), 0, 0, &outputSet, 1);
   if (MB_SUCCESS != rval)
     std::cout << "can't write output\n";
