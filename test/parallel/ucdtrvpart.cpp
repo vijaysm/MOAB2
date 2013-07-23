8b568e1ce64b2327571c87fc29c4dc799b5c2258
Dan Wu <wuda@mcs.anl.gov>
2013-07-23 13:48:50 -0500

    Check some values of tag T0 in unit tests read_ucd_nc (serial) and ucdtrvpart (parallel) for HOMME. A bug inside NCHelper::convert_variable() (fixed by revision d018f20) would fail the parallel test ucdtrvpart.

--- test/parallel/ucdtrvpart.cpp
+++ test/parallel/ucdtrvpart.cpp
@@ -19,7 +19,7 @@ void test_multiple_loads_of_same_file();
 
 std::string partition_method;
 
-int main(int argc, char **argv)
+int main(int argc, char* argv[])
 {
   MPI_Init(&argc, &argv);
   int result = 0;
@@ -32,12 +32,11 @@ int main(int argc, char **argv)
   return result;
 }
 
-
 void test_read_parallel_ucd_trivial()
 {
-  // disable spectral mesh for the time being, it is not ready yet
-  partition_method = std::string(";PARTITION_METHOD=TRIVIAL_PARTITION;PARALLEL_RESOLVE_SHARED_ENTS");
+  // Disable spectral mesh for the time being, it is not ready yet
   //partition_method = std::string(";PARTITION_METHOD=TRIVIAL_PARTITION;SPECTRAL_MESH;PARALLEL_RESOLVE_SHARED_ENTS");
+  partition_method = std::string(";PARTITION_METHOD=TRIVIAL_PARTITION;PARALLEL_RESOLVE_SHARED_ENTS");
   test_read_parallel(3458, true);
 }
   
@@ -56,8 +55,7 @@ void test_read_parallel(int num_verts, bool test_nb_nodes)
   rval = mb.create_meshset(MESHSET_SET, file_set);
   CHECK_ERR(rval);
 
-  std::string opt = std::string("PARALLEL=READ_PART") +
-      partition_method;
+  std::string opt = std::string("PARALLEL=READ_PART") + partition_method;
   rval = mb.load_file(example, &file_set, opt.c_str());
   CHECK_ERR(rval);
 
@@ -66,34 +64,35 @@ void test_read_parallel(int num_verts, bool test_nb_nodes)
   rval = pcomm->check_all_shared_handles();
   CHECK_ERR(rval);
 
-    // get the total # owned verts
+  // Get the total # owned verts
   Range verts;
   rval = mb.get_entities_by_type(0, MBVERTEX, verts);
   CHECK_ERR(rval);
   rval = pcomm->filter_pstatus(verts, PSTATUS_NOT_OWNED, PSTATUS_NOT);
   CHECK_ERR(rval);
-  int proc=pcomm->proc_config().proc_rank();
-  if (0==proc)
-  {
-    // remove from verts the gather set ents
+
+  int rank = pcomm->proc_config().proc_rank();
+  if (0 == rank) {
+    // Remove from verts the gather set ents
     EntityHandle gather_set;
     Range gth_ents;
     rval = Util::gather_set_entities(&mb, gather_set, gth_ents);
     CHECK_ERR(rval);
     verts = subtract(verts, gth_ents);
   }
+
   int my_num = verts.size(), total_verts;
-  std::cout<<"proc: " << pcomm->proc_config().proc_rank() << " verts:" << my_num << "\n";
+  std::cout << "proc: " << rank << " verts:" << my_num << "\n";
   MPI_Reduce(&my_num, &total_verts, 1, MPI_INTEGER, MPI_SUM, 0, pcomm->proc_config().proc_comm());
   
-  if (0 == pcomm->proc_config().proc_rank())
-  {
-    std::cout<<"total vertices: " << total_verts << "\n";
+  if (0 == rank) {
+    std::cout << "total vertices: " << total_verts << "\n";
     if (test_nb_nodes)
       CHECK_EQUAL(total_verts, num_verts);
   }
+
   std::string write_options("PARALLEL=WRITE_PART;");
-  mb.write_file( "test.h5m", NULL, write_options.c_str() );
+  mb.write_file("test.h5m", NULL, write_options.c_str());
 }
 
 void test_multiple_loads_of_same_file()
@@ -105,7 +104,7 @@ void test_multiple_loads_of_same_file()
   rval = mb.create_meshset(MESHSET_SET, file_set);
   CHECK_ERR(rval);
 
-  // read first only header information, no mesh, no variable
+  // Read first only header information, no mesh, no variable
   std::string opts("PARALLEL=READ_PART;PARTITION;NOMESH;VARIABLE=;PARTITION_METHOD=TRIVIAL_PARTITION");
   rval = mb.load_file(example, &file_set, opts.c_str());
   CHECK_ERR(rval);
@@ -114,8 +113,53 @@ void test_multiple_loads_of_same_file()
   rval = mb.load_file(example, &file_set, opts.c_str());
   CHECK_ERR(rval);
 
-
   opts = "PARALLEL=READ_PART;PARTITION;PARTITION_METHOD=TRIVIAL_PARTITION;NOMESH;VARIABLE=T;TIMESTEP=0";
   rval = mb.load_file(example, &file_set, opts.c_str());
   CHECK_ERR(rval);
+
+  // Check values of tag T0 (first level) at some strategically chosen places below
+  ParallelComm* pcomm = ParallelComm::get_pcomm(&mb, 0);
+  int procs = pcomm->proc_config().proc_size();
+
+  // Make check runs this test in two processors
+  if (2 == procs) {
+    Range verts;
+    rval = mb.get_entities_by_type(0, MBVERTEX, verts);
+    CHECK_ERR(rval);
+
+    int rank = pcomm->proc_config().proc_rank();
+    if (0 == rank) {
+      // Remove from verts the gather set ents
+      EntityHandle gather_set;
+      Range gth_ents;
+      rval = Util::gather_set_entities(&mb, gather_set, gth_ents);
+      CHECK_ERR(rval);
+      verts = subtract(verts, gth_ents);
+    }
+    CHECK_EQUAL((size_t)1825, verts.size());
+
+    Tag Ttag0;
+    rval = mb.tag_get_handle("T0", 26, MB_TYPE_DOUBLE, Ttag0, MB_TAG_DENSE);
+    CHECK_ERR(rval);
+
+    int count;
+    void* Tbuf;
+    rval = mb.tag_iterate(Ttag0, verts.begin(), verts.end(), count, Tbuf);
+    CHECK_ERR(rval);
+    CHECK_EQUAL((size_t)count, verts.size());
+
+    const double eps = 0.0001;
+    double* data = (double*) Tbuf;
+
+    if (0 == rank) {
+      CHECK_REAL_EQUAL(233.1136, data[0 * 26], eps); // First vert
+      CHECK_REAL_EQUAL(237.1977, data[912 * 26], eps); // Median vert
+      CHECK_REAL_EQUAL(234.9711, data[1824 * 26], eps); // Last vert
+    }
+    else if (1 == rank) {
+      CHECK_REAL_EQUAL(233.1136, data[0 * 26], eps); // First vert
+      CHECK_REAL_EQUAL(231.0446, data[912 * 26], eps); // Median vert
+      CHECK_REAL_EQUAL(234.0416, data[1824 * 26], eps); // Last vert
+    }
+  }
 }
