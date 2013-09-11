773c1b6242aabe655fa9ac6e4741f176462a68f1
Tim Tautges <tautges@mcs.anl.gov>
2013-09-11 16:50:53 -0500

    These changes hadn't made it into my previous commits for some reason.

--- src/AdaptiveKDTree.cpp
+++ src/AdaptiveKDTree.cpp
@@ -1956,23 +1956,15 @@ namespace moab {
                   }
                   tris_out[w] = *iter;
                   dists_out[w] = tri_t;
-                  ray_end = dists_out.back();
+                  if (tris_out.size() >= (unsigned)max_ints)
+                      // when we have already reached the max intx points, we cans safely reset
+                      // ray_end, because we will accept new points only "closer" than the last one
+                    ray_end = dists_out.back();
                 }
               }
-              int w = tris_out.size() - 1;
-              for (; w > 0 && tri_t < dists_out[w-1]; --w) {
-                tris_out[w] = tris_out[w-1];
-                dists_out[w] = dists_out[w-1];
-              }
-              tris_out[w] = *iter;
-              dists_out[w] = tri_t;
-              if (tris_out.size() >= (unsigned)max_ints)
-                // when we have already reached the max intx points, we cans safely reset
-                // ray_end, because we will accept new points only "closer" than the last one
-                ray_end = dists_out.back();
             }
           }
-
+          
           continue;
         }
     
