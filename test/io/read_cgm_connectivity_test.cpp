62b903bdb19123e497fbda71ebdc7df098ed51aa
Pshriwise <shriwise@wisc.edu>
2014-01-28 19:33:36 -0600

    Added a test for triangle-edge coincidence in the case of a cube read by ReadCGM

--- test/io/read_cgm_connectivity_test.cpp
+++ test/io/read_cgm_connectivity_test.cpp
@@ -31,7 +31,10 @@ void read_file( Interface* moab, const char* input_file );
 // List of tests in this file
 void cube_verts_connectivity_test();
 void cube_tris_connectivity_test();
+void cube_tri_curve_coincidence_test();
 
+//Other functions
+ErrorCode match_tri_edges_w_curve( Interface* moab, Range tri_edges, Range curves);
 
 int main(int /* argc */, char** /* argv */)
 {
@@ -39,6 +42,7 @@ int main(int /* argc */, char** /* argv */)
  
   result += RUN_TEST(cube_verts_connectivity_test);
   result += RUN_TEST(cube_tris_connectivity_test);
+  result += RUN_TEST(cube_tri_curve_coincidence_test);
  
   return result;
 }
@@ -84,7 +88,7 @@ void cube_verts_connectivity_test()
 
 void cube_tris_connectivity_test()
 {
-    ErrorCode rval;
+  ErrorCode rval;
   //Open the test file
   Core moab;
   Interface* mb = &moab;
@@ -108,3 +112,58 @@ void cube_tris_connectivity_test()
 
 }
 
+void cube_tri_curve_coincidence_test()
+{
+
+  ErrorCode rval;
+  //Open the test file
+  Core moab;
+  Interface* mb = &moab;
+  read_file( mb, input_cube);
+
+  //Get curves from the mesh
+  Range curves;
+  rval = mb->get_entities_by_type( 0, MBEDGE, curves);
+  CHECK_ERR(rval);
+
+  //Get triangles from the mesh
+  Range tris;
+  rval = mb->get_entities_by_type( 0, MBTRI, tris);
+  CHECK_ERR(rval);
+
+  for(Range::const_iterator i=tris.begin(); i!=tris.end(); i++)
+    {
+      //Get the any curve edges that are a part of the triangle
+      Range tri_edges;
+      rval = mb->get_adjacencies( &(*i), 1, 1, false, tri_edges);
+      CHECK_ERR(rval);
+
+      int num_of_tri_edges = tri_edges.size();
+      CHECK_EQUAL(2, num_of_tri_edges);
+      rval = match_tri_edges_w_curve( mb, tri_edges, curves);
+      CHECK_ERR(rval);
+  
+    }
+
+}
+
+ErrorCode match_tri_edges_w_curve( Interface* moab, Range tri_edges, Range curves)
+{
+
+  ErrorCode rval;
+  int match_counter;
+  std::cout << "Size of curves = " << curves.size() << std::endl;
+  for(Range::const_iterator i=tri_edges.begin(); i!=tri_edges.end(); i++)
+    {
+      for(Range::const_iterator j=curves.begin(); j!=curves.end(); j++)
+	{
+	  std::cout << "Tri edge ID = " <<  *i << std::endl;
+	  std::cout << "Curve edge ID = " << *j << std::endl;
+          if( *i  == *j  ) match_counter++;
+	}
+    }
+
+  int num_of_tri_edges = tri_edges.size();
+  CHECK_EQUAL( num_of_tri_edges, match_counter );
+  return MB_SUCCESS;
+} 
