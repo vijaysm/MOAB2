d31d5b268fe8f83a0d969fbffac03155d11f2044
Pshriwise <shriwise@wisc.edu>
2014-02-06 11:52:27 -0600

    Added new ReadCGM test file read_cgm_group_test.cpp. Included this in the /test/io Makefile.am

---
+++ test/io/read_cgm_group_test.cpp
@@ -0,0 +1,51 @@
+
+#include <iostream>
+#include "moab/Interface.hpp"
+#ifndef IS_BUILDING_MB
+#define IS_BUILDING_MB
+#endif
+#include "TestUtil.hpp"
+#include "Internals.hpp"
+#include "moab/Core.hpp"
+#include "MBTagConventions.hpp"
+#include "InitCGMA.hpp"
+#include "GeometryQueryTool.hpp"
+
+using namespace moab;
+
+#define CHKERR(A) do { if (MB_SUCCESS != (A)) { \
+  std::cerr << "Failure (error code " << (A) << ") at " __FILE__ ":" \
+            << __LINE__ << std::endl; \
+  return A; } } while(false)
+
+
+#ifdef MESHDIR
+static const char input_cube[] = STRINGIFY(MESHDIR) "/io/cube.sat";
+#else
+static const char input_cube[] = "/io/cube.sat";
+#endif
+
+// Function used to load the test file
+void read_file( Interface* moab, const char* input_file );
+
+// List of tests in this file
+
+
+
+int main(int /* argc */, char** /* argv */)
+{
+  int result = 0;
+ 
+  return result;
+}
+
+
+
+void read_file( Interface* moab, const char* input_file )
+{
+  InitCGMA::initialize_cgma();
+  GeometryQueryTool::instance()->delete_geometry();
+
+  ErrorCode rval = moab->load_file( input_file );
+  CHECK_ERR(rval);
+}
