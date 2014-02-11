8b2ad157ff46c1011d31a2b3f08c1fe779c27e69
Andrew Davis <davisa@engr.wisc.edu>
2014-02-10 18:28:24 -0600

    added dagmc test driver file

---
+++ test/dagmc/dagmc_test.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "moab/Interface.hpp"
+#ifndef IS_BUILDING_MB
+#define IS_BUILDING_MB
+#endif
+#include "TestUtil.hpp"
+#include "Internals.hpp"
+#include "moab/Core.hpp"
+
+#include "DagMC.hpp"
+
+using namespace moab;
+
+using moab::DagMC;
+
+#define DAG DagMC::instance()
+
+#define CHKERR(A) do { if (MB_SUCCESS != (A)) { \
+  std::cerr << "Failure (error code " << (A) << ") at " __FILE__ ":" \
+            << __LINE__ << std::endl; \
+  return A; } } while(false)
+
+
+void dagmc_load_file() 
+{
+  ErrorCode rval = DAG->load_file("test_geom.h5m"); // open the Dag file
+  CHECK_ERR(rval);
+}
+
+void dagmc_build_obb() 
+{
+  ErrorCode rval = DAG->init_OBBTree();
+  CHECK_ERR(rval);
+}
+  
+  
+int main(int /* argc */, char** /* argv */)
+{
+  int result = 0;
+  result += RUN_TEST( dagmc_load_file ); // test ray fire
+  result += RUN_TEST( dagmc_build_obb ); // build the obb
+
+  return result;
+}
