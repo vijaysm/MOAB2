d72c05490f99057da480387a21b2f02205a8d896
Andrew Davis <davisa@engr.wisc.edu>
2014-02-10 22:13:04 -0600

    added more tests

--- test/dagmc/dagmc_test.cpp
+++ test/dagmc/dagmc_test.cpp
@@ -38,13 +38,39 @@ void dagmc_build_obb()
   ErrorCode rval = DAG->init_OBBTree();
   CHECK_ERR(rval);
 }
-  
+
+void dagmc_num_vols()
+{
+  int num_vols = DAG->num_entities(3); 
+  CHECK_EQUAL(2,num_vols);
+}
+
+void dagmc_entity_handle()
+{
+  int num_vols = DAG->num_entities(3); 
+  EntityHandle vol;
+  for ( int i = 0 ; i < num_vols ; i++ )
+    {
+      vol = DAG->entity_by_index(3,i);
+    }
+  CHECK_EQUAL(12682136550675316765,vol);
+}
+
+void dagmc_point_in()
+{
+  int result = 0;
+  double xyz[3]={0.0,0.0,0.0};
+  ErrorCode = DAG->point_in_volume(12682136550675316765,pos,&result);
+  CHECK_EQUAL(1,result);
+}
   
 int main(int /* argc */, char** /* argv */)
 {
   int result = 0;
   result += RUN_TEST( dagmc_load_file ); // test ray fire
   result += RUN_TEST( dagmc_build_obb ); // build the obb
-
+  result += RUN_TEST( dagmc_num_vols  ); // make sure the num of vols correct
+  result += RUN_TEST( dagmc_entity_handle); // check the entity handle correct
+  result += RUN_TEST( dagmc_point_in);
   return result;
 }
