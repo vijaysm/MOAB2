ef89eeecde578329e6d3ac28a1197a058990ab79
Andrew Davis <davisa@engr.wisc.edu>
2014-02-12 09:20:10 -0600

    added more dagmc test to simple tests

--- test/dagmc/dagmc_test.cpp
+++ test/dagmc/dagmc_test.cpp
@@ -49,28 +49,78 @@ void dagmc_entity_handle()
 {
   int num_vols = DAG->num_entities(3); 
   EntityHandle vol;
+  EntityHandle volume = 12682136550675316765;
   for ( int i = 0 ; i < num_vols ; i++ )
     {
       vol = DAG->entity_by_index(3,i);
     }
-  CHECK_EQUAL(12682136550675316765,vol);
+  CHECK_EQUAL(volume,vol);
 }
 
 void dagmc_point_in()
 {
   int result = 0;
   double xyz[3]={0.0,0.0,0.0};
-  ErrorCode rval = DAG->point_in_volume(12682136550675316765,xyz,result);
+  EntityHandle volume = 12682136550675316765;
+  ErrorCode rval = DAG->point_in_volume(volume,xyz,result);
   CHECK_EQUAL(1,result);
+}
+
+void dagmc_rayfire()
+{
+  const double eps = 1e-6; // epsilon for test, faceting tol?
+
+  // note model is cube of side 10, centred at 0,0,0, so ray fire along
+  // any unit direction should be exactly 5.0
+  double xyz[3]={0.0,0.0,0.0};
+  double dir[3]={0.0,0.0,1.0}; 
+  EntityHandle next_surf;
+  double next_surf_dist;
+  EntityHandle volume = 12682136550675316765;
+
+  ErrorCode rval = DAG->ray_fire(volume,xyz,dir,next_surf,next_surf_dist);
+  CHECK_REAL_EQUAL(5.0,next_surf_dist,eps); 
+}
+
+void dagmc_closest_to()
+{
+  const double eps = 1e-6; // epsilon for test, faceting tol?
+
+  // note model is cube of side 10, centred at 0,0,0, so ray fire along
+  // any unit direction should be exactly 5.0
+  double xyz[3]={-6.0,0.0,0.0};
+  double distance; // distance from point to nearest surface
+  EntityHandle volume = 12682136550675316765;
+
+  ErrorCode rval = DAG->closest_to_location(volume,xyz,distance);
+  // distance should be 1.0 cm
+  CHECK_REAL_EQUAL(1.0,distance,eps); 
+}
+
+void dagmc_test_boundary()
+{
+  EntityHandle volume = 12682136550675316765;
+  EntityHandle surf   = 12682136550675316759;
+  double xyz[3]={0.0,0.0,5.0};
+  double dir[3]={0.0,0.0,1.0}; 
+  int result;
+  
+  ErrorCode rval = DAG->test_volume_boundary(volume,surf,xyz,dir,result);
+  // check ray leaving volume
+  CHECK_EQUAL(0,result);  
 }
   
 int main(int /* argc */, char** /* argv */)
 {
   int result = 0;
-  result += RUN_TEST( dagmc_load_file ); // test ray fire
-  result += RUN_TEST( dagmc_build_obb ); // build the obb
-  result += RUN_TEST( dagmc_num_vols  ); // make sure the num of vols correct
-  result += RUN_TEST( dagmc_entity_handle); // check the entity handle correct
-  result += RUN_TEST( dagmc_point_in);
+  result += RUN_TEST( dagmc_load_file );     // test ray fire
+  result += RUN_TEST( dagmc_build_obb );     // build the obb
+  result += RUN_TEST( dagmc_num_vols  );     // make sure the num of vols correct
+  result += RUN_TEST( dagmc_entity_handle);  // check the entity handle correct
+  result += RUN_TEST( dagmc_point_in);       // check entity by point
+  result += RUN_TEST( dagmc_rayfire ) ;      // ensure ray fire distance is correct
+  result += RUN_TEST( dagmc_closest_to );    // check the distance to surface nearest point
+  result += RUN_TEST( dagmc_test_boundary ); // check particle entering leaving
+
   return result;
 }
