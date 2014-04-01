29ccf3e7ac294ab7e95144e3e590bc73dd7cac40
Dan Wu <wuda@mcs.anl.gov>
2014-04-01 16:11:23 -0500

    Minor changes to WriteNC.cpp (code formatting, remove some compiler warnings). Added a new test for writing variable gw (gauss weights). Running this test shows "Writer with name NC for file testgw.nc using extension nc was unsuccessful. Using default writer WriteHDF5Parallel for file testgw.nc". Will update NC writer later to make it work on variable gw.

--- test/io/write_nc.cpp
+++ test/io/write_nc.cpp
@@ -15,7 +15,8 @@ static const char example_eul[] = "/io/camEul26x48x96.t3.nc";
 #endif
 
 // CAM-EUL
-void test_read_write_one_var();
+void test_read_write_T();
+void test_read_write_gw();
 
 ErrorCode get_options(std::string& opts);
 
@@ -31,7 +32,8 @@ int main(int argc, char* argv[])
   argv[0] = argv[argc - argc]; // To remove the warnings in serial mode about unused variables
 #endif
 
-  result += RUN_TEST(test_read_write_one_var);
+  result += RUN_TEST(test_read_write_T);
+  result += RUN_TEST(test_read_write_gw);
 
 #ifdef USE_MPI
   fail = MPI_Finalize();
@@ -42,7 +44,7 @@ int main(int argc, char* argv[])
   return result;
 }
 
-void test_read_write_one_var()
+void test_read_write_T()
 {
   Core moab;
   Interface& mb = moab;
@@ -51,13 +53,13 @@ void test_read_write_one_var()
   ErrorCode rval = get_options(orig);
   CHECK_ERR(rval);
 
-  // we will have to read without mesh and without var first, to create some tags that we need
+  // We will have to read without mesh and without var first, to create some tags that we need
   // later when we write
   std::string opts;
   opts = orig + std::string(";DEBUG_IO=3;NOMESH;VARIABLE=");
 
   // Need a set for nomesh to work right
-  // this set will have as tags a lot of header information, that will be used for writing back the file
+  // This set will have as tags a lot of header information, that will be used for writing back the file
   EntityHandle set;
   rval = mb.create_meshset(MESHSET_SET, set);
   CHECK_ERR(rval);
@@ -65,15 +67,50 @@ void test_read_write_one_var()
   rval = mb.load_file(example_eul, &set, opts.c_str());
   CHECK_ERR(rval);
 
-  // load one variable, and the mesh
+  // Load variable T, and the mesh
   opts= orig + std::string(";DEBUG_IO=3;VARIABLE=T");
   rval = mb.load_file(example_eul, &set, opts.c_str());
   CHECK_ERR(rval);
 
-  // first test will write information about one variable
+  // This test will write information about variable T
   std::string writeopts;
   writeopts = std::string(";;VARIABLE=T;DEBUG_IO=2;");
   rval = mb.write_file( "testT.nc", 0, writeopts.c_str(), &set, 1);
+  CHECK_ERR(rval);
+}
+
+void test_read_write_gw()
+{
+  Core moab;
+  Interface& mb = moab;
+
+  std::string orig;
+  ErrorCode rval = get_options(orig);
+  CHECK_ERR(rval);
+
+  // We will have to read without mesh and without var first, to create some tags that we need
+  // later when we write
+  std::string opts;
+  opts = orig + std::string(";DEBUG_IO=3;NOMESH;VARIABLE=");
+
+  // Need a set for nomesh to work right
+  // This set will have as tags a lot of header information, that will be used for writing back the file
+  EntityHandle set;
+  rval = mb.create_meshset(MESHSET_SET, set);
+  CHECK_ERR(rval);
+
+  rval = mb.load_file(example_eul, &set, opts.c_str());
+  CHECK_ERR(rval);
+
+  // Load variable gw (gauss weights), and the mesh
+  opts= orig + std::string(";DEBUG_IO=3;VARIABLE=gw");
+  rval = mb.load_file(example_eul, &set, opts.c_str());
+  CHECK_ERR(rval);
+
+  // First test will write information about variable gw
+  std::string writeopts;
+  writeopts = std::string(";;VARIABLE=gw;DEBUG_IO=2;");
+  rval = mb.write_file( "testgw.nc", 0, writeopts.c_str(), &set, 1);
   CHECK_ERR(rval);
 }
 
