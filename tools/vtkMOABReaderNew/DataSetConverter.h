f34c493671a46c8e3aaa01943163256ed48852f0
Jacob Becker <jacob.becker@kitware.com>
2014-04-04 19:10:51 -0400

    update vtk moab
    I updated the vtk moab to match https://github.com/robertmaynard/Sandbox.
    I did one small change to fix a bug with reading materials

--- tools/vtkMOABReaderNew/DataSetConverter.h
+++ tools/vtkMOABReaderNew/DataSetConverter.h
@@ -2,20 +2,16 @@
 #define __smoab_DataSetConverter_h
 
 #include "SimpleMoab.h"
-#include "CellTypeToType.h"
-#include "MixedCellConnectivity.h"
+#include "CellSets.h"
+#include "detail/LoadGeometry.h"
+#include "detail/ReadSparseTag.h"
 
-#include <vtkCellArray.h>
 #include <vtkCellData.h>
 #include <vtkDoubleArray.h>
 #include <vtkFieldData.h>
 #include <vtkIntArray.h>
 #include <vtkIdTypeArray.h>
 #include <vtkNew.h>
-#include <vtkPointData.h>
-#include <vtkPoints.h>
-#include <vtkUnsignedCharArray.h>
-#include <vtkUnstructuredGrid.h>
 
 #include <algorithm>
 
@@ -35,16 +31,12 @@ public:
     Moab(interface.Moab),
     Tag(tag),
     ReadMaterialIds(false),
-    ReadProperties(false),
-    MaterialName("Material")
+    ReadProperties(false)
     {
     }
 
   void readMaterialIds(bool add) { this->ReadMaterialIds = add; }
   bool readMaterialIds() const { return this->ReadMaterialIds; }
-
-  void materialIdName(const std::string& name) { this->MaterialName = name; }
-  const std::string& materialIdName() const { return this->MaterialName; }
 
   void readProperties(bool readProps) { this->ReadProperties = readProps; }
   bool readProperties() const { return this->ReadProperties; }
@@ -54,109 +46,65 @@ public:
   //grid. Currently doesn't support reading properties.
   //Will read in material ids,  if no material id is assigned to an entity,
   //its cells will be given an unique id
+  template<typename VTKGridType>
   bool fill(const smoab::Range& entities,
-            vtkUnstructuredGrid* grid) const
+            VTKGridType* grid) const
     {
     //create a helper datastructure which can determines all the unique point ids
     //and converts moab connecitvity info to vtk connectivity
-    moab::Range cells;
 
-    //append all the entities cells together into a single range
+
+    //get all the cells for each parent entity and create
+    // an entity set of those items
+    int dim = this->Tag->value();
     typedef smoab::Range::const_iterator iterator;
+    smoab::CellSets entitySets;
     for(iterator i=entities.begin(); i!= entities.end(); ++i)
       {
+      smoab::Range entitiesCells;
       if(this->Tag->isComparable())
         {
         //if we are comparable only find the cells that match our tags dimension
-        smoab::Range entitiesCells = this->Interface.findEntitiesWithDimension(*i,Tag->value());
-        cells.insert(entitiesCells.begin(),entitiesCells.end());
+        entitiesCells = this->Interface.findEntitiesWithDimension(*i,dim,true);
         }
       else
         {
-        //this is a bad representation of all other tags, but we are presuming that
-        //neuman and dirichlet are on entitysets with no children
-        this->Moab->get_entities_by_handle(*i,cells);
+        entitiesCells = this->Interface.findHighestDimensionEntities(*i,true);
         }
+      smoab::CellSet set(*i,entitiesCells);
+      entitySets.push_back(set);
       }
 
-    smoab::Range points;
-    this->loadCellsAndPoints(cells,points,grid);
+    moab::Range cells = smoab::getAllCells(entitySets);
+
+
+    //convert the datastructure from a list of cells to a vtk data set
+    detail::LoadGeometry loadGeom(cells,dim,this->Interface);
+    loadGeom.fill(grid);
 
     if(this->readMaterialIds())
       {
-      typedef std::vector<smoab::EntityHandle>::const_iterator EntityHandleIterator;
-      typedef std::vector<int>::const_iterator IdConstIterator;
-      typedef std::vector<int>::iterator IdIterator;
 
-      std::vector<smoab::EntityHandle> searchableCells;
-      searchableCells.reserve(grid->GetNumberOfCells());
-      std::copy(cells.begin(),cells.end(),std::back_inserter(searchableCells));
-      cells.clear(); //release memory we don't need
+      detail::ReadSparseTag materialTagReading(entitySets,
+                                               cells,
+                                               this->Interface);
 
-
-      std::vector<int> materialIds(entities.size());
-      //first off iterate the entities and determine which ones
-      //have moab material ids
-
-        //wrap this area with scope, to remove local variables
-      {
-        smoab::MaterialTag tag;
-        IdIterator materialIndex = materialIds.begin();
-        for(iterator i=entities.begin();
-            i != entities.end();
-            ++i, ++materialIndex)
-          {
-          moab::Tag mtag = this->Interface.getMoabTag(tag);
-
-          int value=-1;
-          this->Moab->tag_get_data(mtag,&(*i),1,&value);
-          *materialIndex=static_cast<int>(value);
-          }
-
-        //now determine ids for all entities that don't have materials
-        IdConstIterator maxPos = std::max_element(materialIds.begin(),
-                                                 materialIds.end());
-        int maxMaterial = *maxPos;
-        for(IdIterator i=materialIds.begin(); i!= materialIds.end(); ++i)
-          {
-          if(*i==-1)
-            {
-            *i = ++maxMaterial;
-            }
-          }
+      smoab::MaterialTag mtag;
+      vtkNew<vtkIntArray> materials;
+      materials->SetName(mtag.name());
+      materialTagReading.fill(materials.GetPointer(),&mtag);
+      grid->GetCellData()->AddArray(materials.GetPointer());
       }
 
-      //now we create the material field, and set all the values
-      vtkNew<vtkIntArray> materialSet;
-      materialSet->SetName(this->materialIdName().c_str());
-      materialSet->SetNumberOfValues(grid->GetNumberOfCells());
+    //by default we always try to load the default tag
+    detail::ReadSparseTag sTagReading(entitySets,
+                                      cells,
+                                      this->Interface);
 
-      IdConstIterator materialValue = materialIds.begin();
-      for(iterator i=entities.begin(); i!= entities.end(); ++i, ++materialValue)
-        {
-        //this is a time vs memory trade off, I don't want to store
-        //the all the cell ids twice over, lets use more time
-        smoab::Range entitiesCells;
-        if(this->Tag->isComparable())
-          {entitiesCells = this->Interface.findEntitiesWithDimension(*i,Tag->value());}
-        else
-          {this->Moab->get_entities_by_handle(*i,entitiesCells);}
-
-        EntityHandleIterator s_begin = searchableCells.begin();
-        EntityHandleIterator s_end = searchableCells.end();
-        for(iterator j=entitiesCells.begin(); j != entitiesCells.end();++j)
-          {
-          EntityHandleIterator result = std::lower_bound(s_begin,
-                                                         s_end,
-                                                         *j);
-          std::size_t newId = std::distance(s_begin,result);
-          materialSet->SetValue(static_cast<int>(newId), *materialValue);
-          }
-        }
-
-      grid->GetCellData()->AddArray(materialSet.GetPointer());
-
-      }
+    vtkNew<vtkIntArray> sparseTagData;
+    sparseTagData->SetName(this->Tag->name());
+    sTagReading.fill(sparseTagData.GetPointer(),this->Tag);
+    grid->GetCellData()->AddArray(sparseTagData.GetPointer());
 
     return true;
     }
@@ -165,28 +113,32 @@ public:
   //given a single entity handle create a unstructured grid from it.
   //optional third parameter is the material id to use if readMaterialIds
   //is on, and no material sparse tag is found for this entity
+  template<typename VTKGridType>
   bool fill(const smoab::EntityHandle& entity,
-            vtkUnstructuredGrid* grid,
+            VTKGridType* grid,
             const int materialId=0) const
     {
     //create a helper datastructure which can determines all the unique point ids
     //and converts moab connecitvity info to vtk connectivity
-    moab::Range cells;
+
+    smoab::Range cells;
+    int dim = this->Tag->value();
     if(this->Tag->isComparable())
       {
       //if we are comparable only find the cells that match our tags dimension
-      cells = this->Interface.findEntitiesWithDimension(entity,Tag->value());
+      cells = this->Interface.findEntitiesWithDimension(entity,dim,true);
       }
     else
       {
-      //this is a bad representation of all other tags, but we are presuming that
-      //neuman and dirichlet are on entitysets with no children
-      this->Moab->get_entities_by_handle(entity,cells);
+      //load subentities
+      cells = this->Interface.findHighestDimensionEntities(entity,true);
       }
 
-    smoab::Range points;
-    this->loadCellsAndPoints(cells,points,grid);
+    //convert the datastructure from a list of cells to a vtk data set
+    detail::LoadGeometry loadGeom(cells,dim,this->Interface);
+    loadGeom.fill(grid);
 
+    const smoab::Range& points = loadGeom.moabPoints();
 
     if(this->readProperties())
       {
@@ -194,53 +146,34 @@ public:
       this->readProperties(points,grid->GetPointData());
       }
 
+    smoab::CellSets cellSets;
+    smoab::CellSet set(entity,cells);
+    cellSets.push_back(set);
     if(this->readMaterialIds())
       {
-      this->readSparseTag(smoab::MaterialTag(),entity,
-                          grid->GetNumberOfCells(),
-                          grid->GetCellData(),
-                          materialId);
+      smoab::MaterialTag mtag;
+      detail::ReadSparseTag materialTagReading(cellSets,
+                                               cells,
+                                               this->Interface);
+
+      vtkNew<vtkIntArray> materials;
+      materials->SetName(mtag.name());
+      materialTagReading.fill(materials.GetPointer(),&mtag);
+      grid->GetCellData()->AddArray(materials.GetPointer());
+
       }
+
+    //by default we always try to load the default tag
+    detail::ReadSparseTag sTagReading(cellSets,
+                                      cells,
+                                      this->Interface);
+
+    vtkNew<vtkIntArray> sparseTagData;
+    sparseTagData->SetName(this->Tag->name());
+    sTagReading.fill(sparseTagData.GetPointer(),this->Tag);
+    grid->GetCellData()->AddArray(sparseTagData.GetPointer());
+
     return true;
-    }
-
-  //----------------------------------------------------------------------------
-  void loadCellsAndPoints(const smoab::Range& cells,
-                          smoab::Range& points,
-                          vtkUnstructuredGrid* grid) const
-    {
-
-    smoab::MixedCellConnectivity mixConn(cells,this->Moab);
-
-    //now that mixConn has all the cells properly stored, lets fixup
-    //the ids so that they start at zero and keep the same logical ordering
-    //as before.
-    vtkIdType numCells, connLen;
-    mixConn.compactIds(numCells,connLen);
-    this->setGridsTopology(mixConn,grid,numCells,connLen);
-
-    mixConn.moabPoints(points);
-
-    vtkNew<vtkPoints> newPoints;
-    this->addCoordinates(points,newPoints.GetPointer());
-    grid->SetPoints(newPoints.GetPointer());
-
-    }
-
-  //----------------------------------------------------------------------------
-  void addCoordinates(smoab::Range pointEntities, vtkPoints* pointContainer) const
-    {
-    //since the smoab::range are always unique and sorted
-    //we can use the more efficient coords_iterate
-    //call in moab, which returns moab internal allocated memory
-    pointContainer->SetDataTypeToDouble();
-    pointContainer->SetNumberOfPoints(pointEntities.size());
-
-    //need a pointer to the allocated vtkPoints memory so that we
-    //don't need to use an extra copy and we can bypass all vtk's check
-    //on out of bounds
-    double *rawPoints = static_cast<double*>(pointContainer->GetVoidPointer(0));
-    this->Moab->get_coords(pointEntities,rawPoints);
     }
 
 private:
@@ -257,36 +190,6 @@ private:
     this->Moab->tag_get_tags_on_entity(entities.front(),tags);
 
     this->readDenseTags(tags,entities,field);
-    }
-
-  //----------------------------------------------------------------------------
-  bool readSparseTag(smoab::Tag tag,
-                      smoab::EntityHandle const& entity,
-                      vtkIdType length,
-                      vtkFieldData* field,
-                      vtkIdType defaultValue) const
-    {
-
-    typedef std::vector<moab::Tag>::const_iterator iterator;
-    moab::Tag mtag = this->Interface.getMoabTag(tag);
-
-    int value=0;
-    moab::ErrorCode rval = this->Moab->tag_get_data(mtag,&entity,1,&value);
-    if(rval!=moab::MB_SUCCESS)
-      {
-      value = defaultValue;
-      }
-
-    vtkNew<vtkIntArray> materialSet;
-    materialSet->SetNumberOfValues(length);
-    materialSet->SetName(this->materialIdName().c_str());
-
-    int *raw = static_cast<int*>(materialSet->GetVoidPointer(0));
-    std::fill(raw,raw+length,value);
-
-    field->AddArray(materialSet.GetPointer());
-
-    return true;
     }
 
   //----------------------------------------------------------------------------
@@ -359,36 +262,7 @@ private:
       }
     }
 
-  //----------------------------------------------------------------------------
-  void setGridsTopology(smoab::MixedCellConnectivity const& mixedCells,
-                vtkUnstructuredGrid* grid,
-                vtkIdType numCells,
-                vtkIdType numConnectivity) const
-    {
-    //correct the connectivity size to account for the vtk padding
-    const vtkIdType vtkConnectivity = numCells + numConnectivity;
 
-    vtkNew<vtkIdTypeArray> cellArray;
-    vtkNew<vtkIdTypeArray> cellLocations;
-    vtkNew<vtkUnsignedCharArray> cellTypes;
-
-    cellArray->SetNumberOfValues(vtkConnectivity);
-    cellLocations->SetNumberOfValues(numCells);
-    cellTypes->SetNumberOfValues(numCells);
-
-    vtkIdType* rawArray = static_cast<vtkIdType*>(cellArray->GetVoidPointer(0));
-    vtkIdType* rawLocations = static_cast<vtkIdType*>(cellLocations->GetVoidPointer(0));
-    unsigned char* rawTypes = static_cast<unsigned char*>(cellTypes->GetVoidPointer(0));
-
-    mixedCells.copyToVtkCellInfo(rawArray,rawLocations,rawTypes);
-
-    vtkNew<vtkCellArray> cells;
-    cells->SetCells(numCells,cellArray.GetPointer());
-    grid->SetCells(cellTypes.GetPointer(),
-                   cellLocations.GetPointer(),
-                   cells.GetPointer(),
-                   NULL,NULL);
-    }
 };
 
 }
