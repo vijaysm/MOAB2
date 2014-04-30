912e5d377ff328dab0e793e65fa840423748fdbe
Iulian Grindeanu <iulian@mcs.anl.gov>
2014-04-30 14:17:09 -0500

    filter out not owned entities, for writing
    modify the test also, as Jayesh
    needs cleanup

--- test/io/write_nc.cpp
+++ test/io/write_nc.cpp
@@ -111,11 +111,13 @@ void test_eul_read_write_T()
   ErrorCode rval = mb.create_meshset(MESHSET_SET, set);
   CHECK_ERR(rval);
 
-  // Load non-set variable T, set variable gw, and the mesh
-  read_opts += ";DEBUG_IO=0;VARIABLE=T,gw";
+  read_opts = "PARALLEL=READ_PART;PARTITION;PARALLEL_RESOLVE_SHARED_ENTS;PARALLEL_GHOSTS=2.0.1;PARTITION_METHOD=SQIJ;VARIABLE=";
   rval = mb.load_file(example_eul, &set, read_opts.c_str());
   CHECK_ERR(rval);
 
+  read_opts = "PARALLEL=READ_PART;PARTITION;PARTITION_METHOD=SQIJ;VARIABLE=T,gw;NOMESH;";
+  rval = mb.load_file(example_eul, &set, read_opts.c_str());
+  CHECK_ERR(rval);
   // Write variables T and gw
   std::string write_opts;
   write_opts = std::string(";;VARIABLE=T,gw;DEBUG_IO=0");
