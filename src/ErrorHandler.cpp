5f907a840c567d841e4cc60721d48b503771e6ab
Dan Wu <wuda@mcs.anl.gov>
2014-07-09 13:20:20 -0500

    Framework code for enhanced error handling.

---
+++ src/ErrorHandler.cpp
@@ -0,0 +1,75 @@
+#include "moab/ErrorHandler.hpp"
+#include "ErrorOutput.hpp"
+#ifdef USE_MPI
+#include "moab_mpi.h"
+#endif
+
+#include <stdlib.h>
+
+namespace moab {
+
+static ErrorOutput* errorOutput = NULL;
+
+void MBErrorHandler_Init()
+{
+  if (NULL == errorOutput) {
+    errorOutput = new ErrorOutput(stderr);
+    errorOutput->use_world_rank();
+  }
+}
+
+void MBErrorHandler_Finalize()
+{
+  if (NULL != errorOutput) {
+    delete errorOutput;
+    errorOutput = NULL;
+  }
+}
+
+bool MBErrorHandler_Initialized()
+{
+  return (NULL != errorOutput);
+}
+
+void MBTraceBackErrorHandler(int line, const char* func, const char* file, const char* dir, const char* err_msg, ErrorType err_type)
+{
+  // For a globally fatal error, get world rank of current processor, so that it is only printed from processor 0
+  // For a per-processor relevant error, set rank of current processor to 0, so that it is always printed
+  int rank = 0;
+  if (MB_ERROR_TYPE_NEW_GLOBAL == err_type && NULL != errorOutput && errorOutput->have_rank())
+    rank = errorOutput->get_rank();
+
+  if (0 == rank) {
+    // Print the error messages if it is a new error
+    if (MB_ERROR_TYPE_EXISTING != err_type && NULL != errorOutput && NULL != err_msg) {
+      errorOutput->print("--------------------- Error Message ------------------------------------\n");
+      errorOutput->printf("%s!\n", err_msg);
+    }
+
+    // Print a line of stack trace
+    if (NULL != errorOutput)
+      errorOutput->printf("%s() line %d in %s%s\n", func, line, dir, file);
+  }
+  else {
+    // Do not print the error messages, since processor 0 will print them
+    // Sleep 10 seconds before aborting so it will not accidently kill process 0
+    sleep(10);
+    abort();
+  }
+}
+
+ErrorCode MBError(int line, const char* func, const char* file, const char* dir, ErrorCode err_code, const char* err_msg, ErrorType err_type)
+{
+  MBTraceBackErrorHandler(line, func, file, dir, err_msg, err_type);
+
+#ifdef USE_MPI
+  // If this is called from the main() routine we call MPI_Abort() to allow
+  // the parallel program to be properly shutdown
+  if (strncmp(func, "main", 4) == 0)
+    MPI_Abort(MPI_COMM_WORLD, err_code);
+#endif
+
+  return err_code;
+}
+
+} // namespace moab
---
+++ src/moab/ErrorHandler.hpp
@@ -0,0 +1,144 @@
+#ifndef MOAB_ERROR_HANDLER_HPP
+#define MOAB_ERROR_HANDLER_HPP
+
+#include "moab/Types.hpp"
+
+#include <sstream>
+#include <string.h>
+
+namespace moab {
+
+//! ErrorType - passed to the error handling routines indicating if this is a new error (globally fatal
+//! or per-processor relevant) or an existing one
+enum ErrorType {MB_ERROR_TYPE_NEW_GLOBAL = 0, MB_ERROR_TYPE_NEW_LOCAL = 1, MB_ERROR_TYPE_EXISTING = 2};
+
+//! Initialize MOAB error handler (e.g. create a utility object for printing error output)
+void MBErrorHandler_Init();
+
+//! Finalize MOAB error handler (e.g. delete the utility object for printing error output)
+void MBErrorHandler_Finalize();
+
+//! Indicates whether MBErrorHandler_Init has been called
+bool MBErrorHandler_Initialized();
+
+//! Routine that is called when an error has been detected
+ErrorCode MBError(int line, const char* func, const char* file, const char* dir,
+                  ErrorCode err_code, const char* err_msg, ErrorType err_type);
+
+#define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+
+#define MBSTRINGIFY_(X) #X
+#define MBSTRINGIFY(X) MBSTRINGIFY_(X)
+
+#ifdef LOCDIR
+#define __SDIR__ MBSTRINGIFY(LOCDIR)
+#else
+#define __SDIR__ ""
+#endif
+
+//! Set a new error with passed error code and passed error message string
+#define SET_ERR(err_code, err_msg) \
+  return MBError(__LINE__, __func__, __FILENAME__, __SDIR__, err_code, err_msg, MB_ERROR_TYPE_NEW_LOCAL)
+
+//! Set a new error with passed error code and passed error message string stream
+#define SET_ERR_STR(err_code, err_msg_str) \
+  do { \
+    std::ostringstream ostr; \
+    ostr << err_msg_str; \
+    return MBError(__LINE__, __func__, __FILENAME__, __SDIR__, err_code, ostr.str().c_str(), MB_ERROR_TYPE_NEW_LOCAL); \
+  } while (false)
+
+//! Set a new global error with passed error code and passed error message string
+#define SET_GLB_ERR(err_code, err_msg) \
+  return MBError(__LINE__, __func__, __FILENAME__, __SDIR__, err_code, err_msg, MB_ERROR_TYPE_NEW_GLOBAL)
+
+//! Set a new global error with passed error code and passed error message string stream
+#define SET_GLB_ERR_STR(err_code, err_msg_str) \
+  do { \
+    std::ostringstream ostr; \
+    ostr << err_msg_str; \
+    return MBError(__LINE__, __func__, __FILENAME__, __SDIR__, err_code, ostr.str().c_str(), MB_ERROR_TYPE_NEW_GLOBAL); \
+  } while (false)
+
+//! Check returned error code against MB_SUCCESS
+#define CHK_ERR(err_code) \
+  do { \
+    if (MB_SUCCESS != err_code) \
+      return MBError(__LINE__, __func__, __FILENAME__, __SDIR__, err_code, "", MB_ERROR_TYPE_EXISTING); \
+  } while (false)
+
+//! Check returned error code against MB_SUCCESS
+//! Set a new error with the returned error code and passed error message string
+#define CHK_ERR1(err_code, err_msg_to_set) \
+  do { \
+    if (MB_SUCCESS != err_code) \
+      SET_ERR(err_code, err_msg_to_set); \
+  } while (false)
+
+//! Check returned error code against MB_SUCCESS
+//! Set a new error with the returned error code and passed error message string stream
+#define CHK_ERR1_STR(err_code, err_msg_str_to_set) \
+  do { \
+    if (MB_SUCCESS != err_code) \
+      SET_ERR_STR(err_code, err_msg_str_to_set); \
+  } while (false)
+
+//! Check returned error code against MB_SUCCESS
+//! Set a new error with passed error code and passed error message string
+#define CHK_ERR2(err_code, err_code_to_set, err_msg_to_set) \
+  do { \
+    if (MB_SUCCESS != err_code) \
+      SET_ERR(err_code_to_set, err_msg_to_set); \
+  } while (false)
+
+//! Check returned error code against MB_SUCCESS
+//! Set a new error with passed error code and passed error message string stream
+#define CHK_ERR2_STR(err_code, err_code_to_set, err_msg_str_to_set) \
+  do { \
+    if (MB_SUCCESS != err_code) \
+      SET_ERR_STR(err_code_to_set, err_msg_str_to_set); \
+  } while (false)
+
+//! Check returned error code against an expected one
+//! Set a new error with default error code and default error message string
+#define CHK_EQL(err_code, exp_err_code) \
+  do { \
+    if (exp_err_code != err_code) \
+      SET_ERR(MB_FAILURE, "Returned error code is not expected"); \
+  } while (false)
+
+//! Check returned error code against an expected one
+//! Set a new error with default error code and passed error message string
+#define CHK_EQL1(err_code, exp_err_code, err_msg_to_set) \
+  do { \
+    if (exp_err_code != err_code) { \
+      SET_ERR(MB_FAILURE, err_msg_to_set); \
+    } \
+  } while (false)
+
+//! Check returned error code against an expected one
+//! Set a new error with default error code and passed error message string stream
+#define CHK_EQL1_STR(err_code, exp_err_code, err_msg_str_to_set) \
+  do { \
+    if (exp_err_code != err_code) \
+      SET_ERR_STR(MB_FAILURE, err_msg_str_to_set); \
+  } while (false)
+
+//! Check returned error code against an expected one
+//! Set a new error with passed error code and passed error message string
+#define CHK_EQL2(err_code, exp_err_code, err_code_to_set, err_msg_to_set) \
+  do { \
+    if (exp_err_code != err_code) \
+      SET_ERR(err_code_to_set, err_msg_to_set); \
+  } while (false)
+
+//! Check returned error code against an expected one
+//! Set a new error with passed error code and passed error message string stream
+#define CHK_EQL2_STR(err_code, exp_err_code, err_code_to_set, err_msg_str_to_set) \
+  do { \
+    if (exp_err_code != err_code) \
+      SET_ERR_STR(err_code_to_set, err_msg_str_to_set); \
+  } while (false)
+#endif
+
+} // namespace moab
