a428f4b71e988e56a76d1c8c011c9a4791f08b99
Vijay Mahadevan <vijaysm@mcs.anl.gov>
2014-10-17 13:06:14 -0500

    Use the pkg-config file for netcdf if available.

--- config/FindNetCDF.cmake
+++ config/FindNetCDF.cmake
@@ -8,57 +8,80 @@
 set (NetCDF_DIR "" CACHE PATH "Path to search for NetCDF header and library files" )
 set (NetCDF_FOUND NO CACHE INTERNAL "Found NetCDF components successfully." )
 
-find_path( NetCDF_INCLUDE_DIR netcdf.h
-  HINTS ${NetCDF_DIR}
-  ${NetCDF_DIR}/include
-  ENV CPLUS_INCLUDE_PATH
-  NO_DEFAULT_PATH
-)
-message (STATUS "---   NetCDF include DIR :: ${NetCDF_INCLUDE_DIR}")
+if (EXISTS "${NetCDF_DIR}/lib/pkgconfig/netcdf.pc")
+  execute_process(COMMAND pkg-config --cflags-only-I "${NetCDF_DIR}/lib/pkgconfig/netcdf.pc" OUTPUT_VARIABLE NetCDF_INCLUDES)
+  execute_process(COMMAND pkg-config --libs-only-L "${NetCDF_DIR}/lib/pkgconfig/netcdf.pc" OUTPUT_VARIABLE NETCDF_LDLAGS)
+  execute_process(COMMAND pkg-config --libs-only-l "${NetCDF_DIR}/lib/pkgconfig/netcdf.pc" OUTPUT_VARIABLE NETCDF_LIBRARY)
+  execute_process(COMMAND pkg-config --libs "${NetCDF_DIR}/lib/pkgconfig/netcdf.pc" OUTPUT_VARIABLE NETCDF_LIBS)
 
-find_library( NetCDF_C_LIBRARY
-  NAMES libnetcdf.a netcdf
-  HINTS ${NetCDF_DIR}
-  ${NetCDF_DIR}/lib64
-  ${NetCDF_DIR}/lib
-  NO_DEFAULT_PATH
-)
+  IF (NOT NetCDF_FOUND)
+    if ( NetCDF_INCLUDES AND NETCDF_LIBS )
+      string(STRIP "${NetCDF_INCLUDES}" NetCDF_INCLUDES)
+      string(STRIP "${NETCDF_LIBS}" NETCDF_LIBS)
+      include (ResolveCompilerPaths)
+      RESOLVE_LIBRARIES(NetCDF_LIBRARIES ${NETCDF_LIBS})
+      SET(NetCDF_LIBRARIES ${NETCDF_LIBS})
+      set(NetCDF_FOUND YES )
+    else ( NetCDF_INCLUDES AND NETCDF_LIBS )
+      set( NetCDF_FOUND NO )
+    endif ( NetCDF_INCLUDES AND NETCDF_LIBS )
+  ENDIF (NOT NetCDF_FOUND)
 
-find_library( NetCDF_CXX_LIBRARY
-  NAMES netcdf_c++
-  HINTS ${NetCDF_DIR}
-  ${NetCDF_DIR}/lib64
-  ${NetCDF_DIR}/lib
-  NO_DEFAULT_PATH
-)
+else (EXISTS "${NetCDF_DIR}/lib/pkgconfig/netcdf.pc")
 
-find_library( NetCDF_FORTRAN_LIBRARY
-  NAMES netcdf_g77 netcdff netcdf_ifc netcdf_x86_64
-  HINTS ${NetCDF_DIR}
-  ${NetCDF_DIR}/lib64
-  ${NetCDF_DIR}/lib
-  NO_DEFAULT_PATH
-)
+  find_path( NetCDF_INCLUDE_DIR netcdf.h
+    HINTS ${NetCDF_DIR}
+    ${NetCDF_DIR}/include
+    ENV CPLUS_INCLUDE_PATH
+    NO_DEFAULT_PATH
+  )
 
-IF (NOT NetCDF_FOUND)
-  if ( NetCDF_INCLUDE_DIR AND NetCDF_C_LIBRARY )
-    set( NetCDF_FOUND YES )
-    set(NetCDF_INCLUDES "${NetCDF_INCLUDE_DIR}")
-    set(NetCDF_LIBRARIES ${NetCDF_C_LIBRARY})
-    if ( NetCDF_CXX_LIBRARY )
-      set(NetCDF_LIBRARIES ${NetCDF_LIBRARIES} ${NetCDF_CXX_LIBRARY})
-    endif ( NetCDF_CXX_LIBRARY )
-    if ( NetCDF_FORTRAN_LIBRARY )
-      set(NetCDF_LIBRARIES ${NetCDF_LIBRARIES} ${NetCDF_FORTRAN_LIBRARY})
-    endif ( NetCDF_FORTRAN_LIBRARY )
-    message (STATUS "---   NetCDF Configuration ::")
-    message (STATUS "        INCLUDES  : ${NetCDF_INCLUDES}")
-    message (STATUS "        LIBRARIES : ${NetCDF_LIBRARIES}")
-  else ( NetCDF_INCLUDE_DIR AND NetCDF_C_LIBRARY )
-    set( NetCDF_FOUND NO )
-    message("finding NetCDF failed, please try to set the var NetCDF_DIR")
-  endif ( NetCDF_INCLUDE_DIR AND NetCDF_C_LIBRARY )
-ENDIF (NOT NetCDF_FOUND)
+  find_library( NetCDF_C_LIBRARY
+    NAMES libnetcdf.a netcdf
+    HINTS ${NetCDF_DIR}
+    ${NetCDF_DIR}/lib64
+    ${NetCDF_DIR}/lib
+    NO_DEFAULT_PATH
+  )
+
+  find_library( NetCDF_CXX_LIBRARY
+    NAMES netcdf_c++
+    HINTS ${NetCDF_DIR}
+    ${NetCDF_DIR}/lib64
+    ${NetCDF_DIR}/lib
+    NO_DEFAULT_PATH
+  )
+
+  find_library( NetCDF_FORTRAN_LIBRARY
+    NAMES netcdf_g77 netcdff netcdf_ifc netcdf_x86_64
+    HINTS ${NetCDF_DIR}
+    ${NetCDF_DIR}/lib64
+    ${NetCDF_DIR}/lib
+    NO_DEFAULT_PATH
+  )
+
+  IF (NOT NetCDF_FOUND)
+    if ( NetCDF_INCLUDE_DIR AND NetCDF_C_LIBRARY )
+      set(NetCDF_FOUND YES )
+      set(NetCDF_INCLUDES "${NetCDF_INCLUDE_DIR}")
+      set(NetCDF_LIBRARIES ${NetCDF_C_LIBRARY})
+      if ( NetCDF_CXX_LIBRARY )
+        set(NetCDF_LIBRARIES ${NetCDF_LIBRARIES} ${NetCDF_CXX_LIBRARY})
+      endif ( NetCDF_CXX_LIBRARY )
+      if ( NetCDF_FORTRAN_LIBRARY )
+        set(NetCDF_LIBRARIES ${NetCDF_LIBRARIES} ${NetCDF_FORTRAN_LIBRARY})
+      endif ( NetCDF_FORTRAN_LIBRARY )
+    else ( NetCDF_INCLUDE_DIR AND NetCDF_C_LIBRARY )
+      set( NetCDF_FOUND NO )
+      message("finding NetCDF failed, please try to set the var NetCDF_DIR")
+    endif ( NetCDF_INCLUDE_DIR AND NetCDF_C_LIBRARY )
+  ENDIF (NOT NetCDF_FOUND)
+
+endif (EXISTS "${NetCDF_DIR}/lib/pkgconfig/netcdf.pc")
+
+message (STATUS "---   NetCDF Configuration ::")
+message (STATUS "        INCLUDES  : ${NetCDF_INCLUDES}")
+message (STATUS "        LIBRARIES : ${NetCDF_LIBRARIES}")
 
 mark_as_advanced(
   NetCDF_DIR
