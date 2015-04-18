0f4e33ec0f5105604cd49860d336130c1d24cb3b
Vijay Mahadevan <vijay.m@gmail.com>
2015-04-18 23:39:18 +0530

    Remove all oldinc headers and tests

--- src/oldinc/MBGeomUtil.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef MBGeomUtil_HEADER
-#define MBGeomUtil_HEADER
-
-#include "MBCartVect.hpp"
-
-#include "moab/GeomUtil.hpp"
-namespace MBGeomUtil = moab::GeomUtil;
-
-#endif
--- src/oldinc/MBmpi.h
+++
@@ -1,4 +0,0 @@
-#ifndef MBmpi_HEADER
-#define MBmpi_HEADER
-#include "moab_mpi.h"
-#endif
