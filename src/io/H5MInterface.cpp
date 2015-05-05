8b232e416c4f2889f0c97050faa1710471556e63
Iulian Grindeanu <iulian@mcs.anl.gov>
2015-05-05 16:03:13 -0500

    use type of element descriptors for counts
    be more precise with the counts
    decide dimension by the number of faces or regions
    (it is 2 if nfaces > 0, and 3 if regions > 0)

--- src/io/H5MInterface.cpp
+++ src/io/H5MInterface.cpp
@@ -36,7 +36,7 @@ int H5MInterface::get_mesh_info( int* verts, int *edges, int*faces, int* regions
   struct mhdf_FileDesc* data;
   /* find PARALLEL_PARTITION tag index */
   const char * pname = "PARALLEL_PARTITION";
-  int i, maxc, ne;
+
   long int nval, junk;
   hid_t table[3];
 
@@ -53,26 +53,25 @@ int H5MInterface::get_mesh_info( int* verts, int *edges, int*faces, int* regions
     return 1;
   }
   *numdim = data->nodes.vals_per_ent;
-  //printf(" num dimensions: %d\n", data->nodes.vals_per_ent);
   *verts = (int)data->nodes.count;
-  //printf(" num vertices:   %d\n", (int)data->nodes.count);
 
-  /* max connectivity number of elem */
-  maxc = -1;
-  ne = 0;
-  for (i=0; i<data->num_elem_desc; i++)
+  for (int i=0; i<data->num_elem_desc; i++)
   {
     struct mhdf_ElemDesc * el_desc = &(data->elems[i]);
     struct mhdf_EntDesc * ent_d = &(el_desc->desc);
-    if (ent_d->vals_per_ent > maxc)
-    {
-      ne = ent_d->count;
-      maxc = ent_d->vals_per_ent;
-    }
+    if (0==strcmp(el_desc->type, mhdf_EDGE_TYPE_NAME)) *edges += ent_d->count;
+    if (0==strcmp(el_desc->type, mhdf_TRI_TYPE_NAME))  *faces += ent_d->count;
+    if (0==strcmp(el_desc->type, mhdf_QUAD_TYPE_NAME)) *faces += ent_d->count;
+    if (0==strcmp(el_desc->type, mhdf_POLYGON_TYPE_NAME)) *faces += ent_d->count;
+    if (0==strcmp(el_desc->type, mhdf_TET_TYPE_NAME)) *regions += ent_d->count;
+    if (0==strcmp(el_desc->type, mhdf_PYRAMID_TYPE_NAME)) *regions += ent_d->count;
+    if (0==strcmp(el_desc->type, mhdf_PRISM_TYPE_NAME)) *regions += ent_d->count;
+    if (0==strcmp(el_desc->type, mdhf_KNIFE_TYPE_NAME)) *regions += ent_d->count;
+    if (0==strcmp(el_desc->type, mdhf_HEX_TYPE_NAME)) *regions += ent_d->count;
+    if (0==strcmp(el_desc->type, mhdf_POLYHEDRON_TYPE_NAME)) *regions += ent_d->count;
+    if (0==strcmp(el_desc->type, mhdf_SEPTAHEDRON_TYPE_NAME)) *regions += ent_d->count;
   }
-  //printf(" num elements:   %d\n", ne);
-  *regions = ne;
-  for (i=0; i<data->num_tag_desc; i++)
+  for (int i=0; i<data->num_tag_desc; i++)
   {
     struct mhdf_TagDesc * tag_desc = &(data->tags[i]);
     if (strcmp(pname,tag_desc->name)==0)
@@ -94,11 +93,13 @@ int H5MInterface::get_mesh_info( int* verts, int *edges, int*faces, int* regions
           return 1;
         }
       }
-    //  printf(" num parallel partitions: %d\n", (int)nval);
+
       *parts = (int)nval;
     }
   }
 
+  if (*faces >0 ) *numdim = 2;
+  if (*regions>0) *numdim = 3;
   mhdf_closeFile( file, &status );
 
   free( data );
