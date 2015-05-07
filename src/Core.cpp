f7a9cbeac5dbf5f37d00a0e6755b1f3289b21b72
Iulian Grindeanu <iulian@mcs.anl.gov>
2015-05-06 22:11:35 -0500

    change Core::create_scd_sequence
    change implementation to use ScdInterface
    it ensures that ScdBoxes are created as needed, and their sets
    
    it is used (AFAIK) only in the test/parallel/scdtest, everybody else uses
    ScdInterface already

--- src/Core.cpp
+++ src/Core.cpp
@@ -4253,10 +4253,22 @@ ErrorCode Core::create_scd_sequence(const HomCoord & coord_min,
 					EntityHandle &  first_handle_out,
 					EntitySequence *&  sequence_out )
 {
-  return sequence_manager()->create_scd_sequence(coord_min, coord_max, type,
+
+  if(!scdInterface)
+    scdInterface = new ScdInterface(this);
+  ScdBox * newBox = NULL;
+  ErrorCode rval = scdInterface->create_scd_sequence(coord_min, coord_max, type,
+                              /*starting_id*/    (int)start_id_hint, newBox); MB_CHK_ERR(rval);
+
+  if (MBVERTEX==type)
+    first_handle_out = newBox->get_vertex(coord_min);
+  else
+    first_handle_out = newBox->get_element(coord_min);
+  return  sequence_manager()->find( first_handle_out, sequence_out );
+  /*return sequence_manager()->create_scd_sequence(coord_min, coord_max, type,
 						 start_id_hint,
 						 first_handle_out,
-						 sequence_out);
+						 sequence_out);*/
 }
 
 ErrorCode Core::add_vsequence(EntitySequence *    vert_seq,
