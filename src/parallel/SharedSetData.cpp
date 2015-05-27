9dce62062204737bbf9a271fe4e29cf3b93f45dd
Dan Wu <wuda@mcs.anl.gov>
2015-05-26 19:41:52 -0500

    Test creating a new ParallelComm instance instead of using an existing one in test/parallel/pcomm_unit.cpp. This updated unit test is expected to fail in a PGI optimization build. Also update src/parallel/SharedSetData.cpp to print an error message before calling abort(), which causes pcomm_unit to fail.

--- src/parallel/SharedSetData.cpp
+++ src/parallel/SharedSetData.cpp
@@ -17,8 +17,10 @@ SharedSetData::SharedSetData(Interface& moab, unsigned rank)
   ErrorCode rval = mb.tag_get_handle( "__sharedSetTag", sizeof(SharedSetTagData), MB_TYPE_OPAQUE,
                                       sharedSetTag, MB_TAG_CREAT|MB_TAG_SPARSE, &zero );
   assert(MB_SUCCESS == rval);
-  if (MB_SUCCESS != rval)
+  if (MB_SUCCESS != rval) {
+    fprintf(stderr, "Aborted from the constructor of SharedSetData.\n");
     abort();
+  }
 }
 
 SharedSetData::~SharedSetData()
