d0effbbf942b747bfa2bea8a59b02d6382fdd3bc
nray <navamitaray@gmail.com>
2015-07-17 17:06:57 -0500

    Fixed the test_adj.

--- test/test_adj.cpp
+++ test/test_adj.cpp
@@ -30,36 +30,84 @@
 #ifndef IS_BUILDING_MB
 #define IS_BUILDING_MB
 #endif
+
 #include "Internals.hpp"
 
 using namespace moab;
+
+#define STRINGIFY_(X) #X
+#define STRINGIFY(X) STRINGIFY_(X)
 
 int main()
 {
 
   Interface* iface = new Core;
 
-  iface->load_mesh( "../test/quad/quad1.gen" );
+  const char* filename = 0;
+
+#ifdef MESHDIR
+#ifdef MOAB_HAVE_HDF5
+  filename = STRINGIFY(MESHDIR) "/testquad-cyl.h5m";
+#else
+  filename = STRINGIFY(MESHDIR) "/testquad-cyl.g";
+#endif
+#else
+#ifdef MOAB_HAVE_HDF5
+  filename = "testquad-cyl.h5m";
+#else
+  filename = "testquad-cyl.g";
+#endif
+#endif
+
+  ErrorCode err;
+  err =  iface->load_mesh( filename);MB_CHK_ERR(err);
+
+  Range quads, verts;
+  err = iface->get_entities_by_dimension(0,0,verts);MB_CHK_ERR(err);
+  err = iface->get_entities_by_dimension(0, 2, quads);MB_CHK_ERR(err);
+
+  for (Range::iterator it = verts.begin(); it != verts.end(); it++)
+    std::cout<<"verts["<<(*it-*verts.begin())<<"] = "<<*it<<std::endl;
+
+  std::vector<EntityHandle> conn;
+  for (Range::iterator it = quads.begin(); it != quads.end(); it++)
+    {
+      conn.clear();
+      err = iface->get_connectivity(&*it, 1, conn);MB_CHK_ERR(err);
+      std::cout<<"verts["<<(*it-*quads.begin())<<"] = "<<*it<<" :: conn = ["<<conn[0]<<", "<<conn[1]<<", "<<conn[2]<<", "<<conn[3]<<"]"<<std::endl;
+    }
 
   std::vector<EntityHandle> nodes;
-  int err;
-  EntityHandle h = CREATE_HANDLE(MBQUAD, MB_START_ID, err);
-  err = iface->get_adjacencies( &h, 1, 0, true, nodes);
+  int error;
+  EntityHandle h = CREATE_HANDLE(MBQUAD, MB_START_ID, error);
 
-  std::vector<EntityHandle> tris;
-  err = iface->get_adjacencies( &h, 1, 1, true, tris);
+  std::cout<<"h = "<<h<<std::endl;
+
+  err = iface->get_adjacencies( &h, 1, 0, true, nodes);MB_CHK_ERR(err);
+
+  for (int i=0; i<(int)nodes.size(); i++)
+    std::cout<<"nodes["<<i<<"] = "<<nodes[i]<<" ";
+  std::cout<<std::endl;
+
+  std::vector<EntityHandle> edgs;
+  err = iface->get_adjacencies( &h, 1, 1, true, edgs);MB_CHK_ERR(err);
+
+  for (int i=0; i<(int)edgs.size(); i++)
+    std::cout<<"edgs["<<i<<"] = "<<edgs[i]<<" ";
+  std::cout<<std::endl;
 
   //faces to nodes
 
-  for(unsigned int i=0; i<tris.size(); i++)
+  for(unsigned int i=0; i<edgs.size(); i++)
   {
-    err = iface->get_adjacencies( &tris[i], 1, 0, true, nodes );
-    std::cout << "edge " << ID_FROM_HANDLE(tris[i]) << std::endl;
-    std::cout << "nodes = ";
-    for(unsigned int j=0; j<nodes.size(); j++)
-      std::cout << nodes[j] << " ";
-    std::cout << std::endl;
-  }
+      nodes.clear();
+      err = iface->get_adjacencies( &edgs[i], 1, 0, true, nodes );MB_CHK_ERR(err);
+      std::cout << "edge " << ID_FROM_HANDLE(edgs[i]) << std::endl;
+      std::cout << "nodes = ";
+      for(unsigned int j=0; j<nodes.size(); j++)
+        std::cout << nodes[j] << " ";
+      std::cout << std::endl;
+    }
 
   delete iface;
 
