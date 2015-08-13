897b52d313cdfaee1e98b88c85eaed3426747f8c
xinglinzhao <xinglin.zhao@stonybrook.edu>
2015-08-13 13:17:38 -0500

    declare member functions as static

--- src/DiscreteGeometry/moab/Solvers.hpp
+++ src/DiscreteGeometry/moab/Solvers.hpp
@@ -9,33 +9,33 @@ namespace moab
     Solvers() {};
     ~Solvers() {};
 
-    void rescale_matrix(int mrows, int ncols, double *V, double *ts);
+    static void rescale_matrix(int mrows, int ncols, double *V, double *ts);
 
-    void gen_vander_bivar(const int npts,const double* us, const int degree, double* V);
+    static void gen_vander_bivar(const int npts,const double* us, const int degree, double* V);
 
-    void compute_qtransposeB(int mrows, int ncols, const double *Q, int bncols, double *bs);
+    static void compute_qtransposeB(int mrows, int ncols, const double *Q, int bncols, double *bs);
 
-    void qr_polyfit_safeguarded(double *V, int mrows, int ncols, double *D, int &rank);
+    static void qr_polyfit_safeguarded(double *V, int mrows, int ncols, double *D, int &rank);
 
-    void backsolve(int mrows, int ncols, double *R, int bncols, double *bs, double *ws);
+    static void backsolve(int mrows, int ncols, double *R, int bncols, double *bs, double *ws);
 
-    void backsolve_polyfit_safeguarded();
+    static void backsolve_polyfit_safeguarded();
 
-    void vec_dotprod(const int len, const double* a, const double* b, double* c);
+    static void vec_dotprod(const int len, const double* a, const double* b, double* c);
 
-    void vec_scalarprod(const int len, const double* a, const double c, double *b);
+    static void vec_scalarprod(const int len, const double* a, const double c, double *b);
 
-    void vec_crossprod(const double a[3], const double b[3], double (&c)[3]);
+    static void vec_crossprod(const double a[3], const double b[3], double (&c)[3]);
 
-    double vec_innerprod(const int len, const double* a, const double* b);
+    static double vec_innerprod(const int len, const double* a, const double* b);
 
-    double vec_2norm(const int len, const double* a);
+    static double vec_2norm(const int len, const double* a);
 
-    double vec_normalize(const int len, const double* a, double* b);
+    static double vec_normalize(const int len, const double* a, double* b);
 
-    void vec_projoff(const int len, const double* a, const double* b, double* c);
+    static void vec_projoff(const int len, const double* a, const double* b, double* c);
 
-    void vec_linear_operation(const int len, const double mu, const double* a, const double psi, const double* b, double* c);
+    static void vec_linear_operation(const int len, const double mu, const double* a, const double psi, const double* b, double* c);
 
 
 
