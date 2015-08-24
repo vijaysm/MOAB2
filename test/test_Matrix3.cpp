b51164bb50d0b856a38f4903da6325a9c77aaee8
pshriwise <shriwise@wisc.edu>
2015-08-24 16:02:11 -0500

    Altered test matrix s.t. it has distinct Eigenvalues. Added test for orthogonality of the Eigenvectors at the end.

--- test/test_Matrix3.cpp
+++ test/test_Matrix3.cpp
@@ -29,15 +29,15 @@ void test_EigenDecomp()
   //Create a matrix
   moab::Matrix3 mat;
 
-  mat(0) = 3;
-  mat(1) = 2;
-  mat(2) = 4;
-  mat(3) = 2;
-  mat(4) = 0;
-  mat(5) = 2;
-  mat(6) = 4;
-  mat(7) = 2;
-  mat(8) = 3;
+  mat(0) = 2;
+  mat(1) = -1;
+  mat(2) = 0;
+  mat(3) = -1;
+  mat(4) = 2;
+  mat(5) = -1;
+  mat(6) = 0;
+  mat(7) = -1;
+  mat(8) = 2;
 
   //now do the Eigen Decomposition of this Matrix
 
@@ -48,11 +48,11 @@ void test_EigenDecomp()
 
   //Hardcoded check values for the results
   double lamda_check[3];
-  lamda_check[0] = 8; lamda_check[1] = -1; lamda_check[2] = -1;
+  lamda_check[0] = 3.41421; lamda_check[1] = 2; lamda_check[2] = 0.585786;
 
-  moab::CartVect vec0_check( 0.666666, 0.3333333, 0.66666666);
-  moab::CartVect vec1_check( 0.596285, 0.298142, -0.745356);
-  moab::CartVect vec2_check( -0.447214, 0.894427, 0); 
+  moab::CartVect vec0_check(0.5, -0.707107, 0.5);
+  moab::CartVect vec1_check(0.707107, 3.37748e-17, -0.707107);
+  moab::CartVect vec2_check(0.5, 0.707107, 0.5); 
   
   //now verfy that the returns Eigenvalues and Eigenvectors are correct (within some tolerance)
   double tol = 1e-04; 
@@ -84,6 +84,12 @@ void test_EigenDecomp()
     moab::CartVect v = moab::Matrix::matrix_vector(mat, vectors[i])-lamda[i]*vectors[i];
     CHECK_REAL_EQUAL( v.length(), 0, tol );
   }
+
+  //for a real, symmetric matrix the Eigenvectors should be orthogonal
+  CHECK_REAL_EQUAL( vectors[0]%vectors[1], 0 , tol );
+  CHECK_REAL_EQUAL( vectors[0]%vectors[2], 0 , tol );
   
   return;
 }
+
+
