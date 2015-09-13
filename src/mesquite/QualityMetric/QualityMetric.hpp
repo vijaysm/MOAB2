38fa3a60b2ac4a9c00874820e46a0a7fb012e0b5
Vijay Mahadevan <vijay.m@gmail.com>
2015-09-13 00:34:23 -0500

    More miscellaneous build fixes

--- src/mesquite/QualityMetric/QualityMetric.hpp
+++ src/mesquite/QualityMetric/QualityMetric.hpp
@@ -49,9 +49,9 @@ Header file for the Mesquite::QualityMetric class
 
 #ifdef _MSC_VER
    typedef unsigned uint32_t;
-#elif defined(MSQ_HAVE_STDINT_H)
+#elif defined(MOAB_HAVE_STDINT_H)
 #  include <stdint.h>
-#elif defined(MSQ_HAVE_INTTYPES_H)
+#elif defined(MOAB_HAVE_INTTYPES_H)
 #  include <inttypes.h>
 #endif
 
