0a8a696c7b80b282bc69a0cbd408f12413b8ccbf
Vijay Mahadevan <vijay.m@gmail.com>
2015-09-24 18:11:02 -0500

    Fixing several warnings in tests

--- test/mbfacet_test.cpp
+++ test/mbfacet_test.cpp
@@ -39,19 +39,7 @@ bool keep_output;
 int number_tests_successful = 0;
 int number_tests_failed = 0;
 
-#define PROCESS_ERROR(A, B)  {if (A!=MB_SUCCESS) {  std::cout << B << std::endl; return 1; } }
-
-#define CHECK( STR ) if (rval != MB_SUCCESS) return print_error( STR, rval, __FILE__, __LINE__ )
-
 using namespace moab;
-
-ErrorCode print_error(const char* desc, ErrorCode rval, const char* file,
-    int line)
-{
-  std::cerr << "ERROR: " << desc << std::endl << "  Error code: " << rval
-      << std::endl << "  At        : " << file << ':' << line << std::endl;
-  return MB_FAILURE; // must always return false or CHECK macro will break
-}
 
 ErrorCode root_set_test(FBEngine * pFacet);
 ErrorCode gentityset_test(FBEngine * pFacet);
@@ -115,8 +103,7 @@ int main(int argc, char *argv[])
   Core mbcore;
   Interface * mb = &mbcore;
 
-  ErrorCode rval = mb->load_file(filename.c_str());
-  PROCESS_ERROR(rval,"failed to load input file");
+  ErrorCode rval = mb->load_file(filename.c_str());MB_CHK_SET_ERR(rval,"failed to load input file");
 
   FBEngine * pFacet = new FBEngine(mb, NULL, true);// smooth facetting, no OBB tree passed
 
@@ -125,8 +112,7 @@ int main(int argc, char *argv[])
 
   // should the init be part of constructor or not?
   // this is where the obb tree is constructed, and smooth faceting initialized, too.
-  rval = pFacet->Init();
-  PROCESS_ERROR(rval,"failed to initialize smoothing");
+  rval = pFacet->Init();MB_CHK_SET_ERR(rval,"failed to initialize smoothing");
 
   std::cout << "root set test: ";
   rval = root_set_test(pFacet);
@@ -176,8 +162,7 @@ int main(int argc, char *argv[])
   std::cout << "\n";
 
   // when we are done, remove modified file if we want to
-  if (!keep_output)
-  {
+  if (!keep_output) {
     remove(filename_out.c_str());
   }
   return number_tests_failed;
@@ -185,10 +170,8 @@ int main(int argc, char *argv[])
 
 ErrorCode root_set_test(FBEngine * pFacet)
 {
-
   EntityHandle root_set;
-  ErrorCode rval = pFacet->getRootSet(&root_set);
-  CHECK( "ERROR : getRootSet failed!" );
+  ErrorCode rval = pFacet->getRootSet(&root_set);MB_CHK_SET_ERR(rval, "ERROR : getRootSet failed!" );
 
   return MB_SUCCESS;
 }
@@ -202,34 +185,29 @@ ErrorCode gentityset_test(FBEngine * pFacet)
   int ent_type = 0; // iBase_VERTEX;
 
   EntityHandle root_set;
-  ErrorCode rval = pFacet->getRootSet(&root_set);
-  CHECK( "ERROR : getRootSet failed!" );
+  ErrorCode rval = pFacet->getRootSet(&root_set);MB_CHK_SET_ERR(rval, "ERROR : getRootSet failed!" );
 
   // get the number of sets in the whole model
   int all_sets = 0;
-  rval = pFacet->getNumEntSets(root_set, 0, &all_sets);
-  CHECK( "Problem getting the number of all gentity sets in whole model." );// why do we count all sets
+  rval = pFacet->getNumEntSets(root_set, 0, &all_sets);MB_CHK_SET_ERR(rval, "Problem getting the number of all gentity sets in whole model." );// why do we count all sets
 
 
   // add gentities to entitysets by type
   for (; ent_type < num_type; ent_type++) {
     // initialize the entityset
     rval = pFacet->createEntSet(1, &ges_array[ent_type]);// 1 means isList, ordered
-    CHECK( "Problem creating entityset." );
+    MB_CHK_SET_ERR(rval, "Problem creating entityset." );
 
     // get entities by type in total "mesh"
     Range gentities;
-    rval = pFacet->getEntities(root_set, ent_type, gentities);
-    CHECK( "Failed to get gentities by type in gentityset_test." );
+    rval = pFacet->getEntities(root_set, ent_type, gentities);MB_CHK_SET_ERR(rval, "Failed to get gentities by type in gentityset_test." );
 
     // add gentities into gentity set
-    rval = pFacet->addEntArrToSet(gentities, ges_array[ent_type]);
-    CHECK( "Failed to add gentities in entityset_test." );
+    rval = pFacet->addEntArrToSet(gentities, ges_array[ent_type]);MB_CHK_SET_ERR(rval, "Failed to add gentities in entityset_test." );
 
     // Check to make sure entity set really has correct number of entities in it
     rval = pFacet->getNumOfType(ges_array[ent_type], ent_type,
-        &number_array[ent_type]);
-    CHECK( "Failed to get number of gentities by type in entityset_test." );
+        &number_array[ent_type]);MB_CHK_SET_ERR(rval, "Failed to get number of gentities by type in entityset_test." );
 
     // compare the number of entities by type
     int num_type_gentity = gentities.size();
@@ -246,44 +224,36 @@ ErrorCode gentityset_test(FBEngine * pFacet)
   // make a super set having all entitysets
   EntityHandle super_set;
   rval = pFacet->createEntSet(1, &super_set);// 1 is list
-  CHECK( "Failed to create a super set in gentityset_test." );
+  MB_CHK_SET_ERR(rval, "Failed to create a super set in gentityset_test." );
 
   for (int i = 0; i < num_type; i++) {
-    rval = pFacet->addEntSet(ges_array[i], super_set);
-    CHECK( "Failed to create a super set in gentityset_test." );
+    rval = pFacet->addEntSet(ges_array[i], super_set);MB_CHK_SET_ERR(rval, "Failed to create a super set in gentityset_test." );
   }
 
   //----------TEST BOOLEAN OPERATIONS----------------//
 
   EntityHandle temp_ges1;
-  rval = pFacet->createEntSet(1, &temp_ges1);
-  CHECK( "Failed to create a super set in gentityset_test." );
+  rval = pFacet->createEntSet(1, &temp_ges1);MB_CHK_SET_ERR(rval, "Failed to create a super set in gentityset_test." );
 
   // Subtract
   // add all EDGEs and FACEs to temp_es1
   // get all EDGE entities
   Range gedges, gfaces, temp_gentities1;
-  rval = pFacet->getEntities(ges_array[1], /* iBase_EDGE*/1, gedges);
-  CHECK( "Failed to get gedge gentities in gentityset_test." );
+  rval = pFacet->getEntities(ges_array[1], /* iBase_EDGE*/1, gedges);MB_CHK_SET_ERR(rval, "Failed to get gedge gentities in gentityset_test." );
 
   // add EDGEs to ges1
-  rval = pFacet->addEntArrToSet(gedges, temp_ges1);
-  CHECK( "Failed to add gedge gentities in gentityset_test." );
+  rval = pFacet->addEntArrToSet(gedges, temp_ges1);MB_CHK_SET_ERR(rval, "Failed to add gedge gentities in gentityset_test." );
 
   // get all FACE gentities
-  rval = pFacet->getEntities(ges_array[2], /*iBase_FACE*/2, gfaces);
-  CHECK( "Failed to get gface gentities in gentityset_test." );
+  rval = pFacet->getEntities(ges_array[2], /*iBase_FACE*/2, gfaces);MB_CHK_SET_ERR(rval, "Failed to get gface gentities in gentityset_test." );
 
   // add FACEs to es1
-  rval = pFacet->addEntArrToSet(gfaces, temp_ges1);
-  CHECK( "Failed to add gface gentities in gentityset_test." );
+  rval = pFacet->addEntArrToSet(gfaces, temp_ges1);MB_CHK_SET_ERR(rval, "Failed to add gface gentities in gentityset_test." );
 
   // subtract EDGEs
-  rval = pFacet->gsubtract(temp_ges1, ges_array[1], temp_ges1);
-  CHECK( "Failed to subtract gentitysets in gentityset_test." );
+  rval = pFacet->gsubtract(temp_ges1, ges_array[1], temp_ges1);MB_CHK_SET_ERR(rval, "Failed to subtract gentitysets in gentityset_test." );
 
-  rval = pFacet->getEntities(temp_ges1, 2, temp_gentities1);
-  CHECK( "Failed to get gface gentities in gentityset_test." );
+  rval = pFacet->getEntities(temp_ges1, 2, temp_gentities1);MB_CHK_SET_ERR(rval, "Failed to get gface gentities in gentityset_test." );
 
   if (gfaces.size() != temp_gentities1.size()) {
     std::cerr
@@ -296,7 +266,7 @@ ErrorCode gentityset_test(FBEngine * pFacet)
   // check there's nothing but gfaces in temp_ges1
   int num_gents;
   rval = pFacet->getNumOfType(temp_ges1, 1, &num_gents);
-  CHECK( "Failed to get dimensions of gentities in gentityset_test." );
+  MB_CHK_SET_ERR(rval, "Failed to get dimensions of gentities in gentityset_test." );
   if (0 != num_gents) {
     std::cerr << "Subtraction failed to remove all edges" << std::endl;
     return MB_FAILURE;
@@ -310,7 +280,7 @@ ErrorCode geometry_evaluation_test(FBEngine * pFacet)
   int i;
   EntityHandle root_set;
   ErrorCode rval = pFacet->getRootSet(&root_set);
-  CHECK( "ERROR : getRootSet failed!" );
+  MB_CHK_SET_ERR(rval, "ERROR : getRootSet failed!" );
 
   int top = 0;// iBase_VERTEX;
   int num_test_top = 4; // iBase_ALL_TYPES;
@@ -320,8 +290,7 @@ ErrorCode geometry_evaluation_test(FBEngine * pFacet)
   //
   for (i = top; i < num_test_top; i++) {
     Range gentities;
-    rval = pFacet->getEntities(root_set, i, gentities);
-    CHECK("Failed to get gentities in eval tests.");
+    rval = pFacet->getEntities(root_set, i, gentities);MB_CHK_SET_ERR(rval,"Failed to get gentities in eval tests.");
 
     gentity_vectors[i].resize(gentities.size());
     std::copy(gentities.begin(), gentities.end(), gentity_vectors[i].begin());
@@ -337,15 +306,14 @@ ErrorCode geometry_evaluation_test(FBEngine * pFacet)
     if (i != 1) {
       for (vit = gentity_vectors[i].begin(); vit != gentity_vectors[i].end(); ++vit) {
         EntityHandle this_gent = *vit;
+
         rval = pFacet->getEntBoundBox(this_gent, &min[0], &min[1], &min[2],
-            &max[0], &max[1], &max[2]);
-        CHECK("Failed to get bounding box of entity.");
+            &max[0], &max[1], &max[2]);MB_CHK_SET_ERR(rval,"Failed to get bounding box of entity.");
 
         for (int j=0; j<3; j++)
           near[j] = (min[j]+max[j])/2.;
         rval = pFacet->getEntClosestPt(this_gent, near[0], near[1], near[2],
-            &on[0], &on[1], &on[2]);
-        CHECK("Failed to get closest point on entity.");
+            &on[0], &on[1], &on[2]);MB_CHK_SET_ERR(rval,"Failed to get closest point on entity.");
       }
     }
     else
@@ -353,18 +321,16 @@ ErrorCode geometry_evaluation_test(FBEngine * pFacet)
       // for edges, provide a little better help
       for (vit = gentity_vectors[i].begin(); vit != gentity_vectors[i].end(); ++vit) {
         EntityHandle this_gent = *vit;
+
         // we know that the edge is parametric, with par between 0 and 1
-
         // some random parameter
-
-        rval = pFacet->getEntUtoXYZ(this_gent, 0.33, near[0], near[1], near[2]);
-        CHECK("Failed to get a new point");
+        rval = pFacet->getEntUtoXYZ(this_gent, 0.33, near[0], near[1], near[2]);MB_CHK_SET_ERR(rval,"Failed to get a new point");
 
         std::cout << " entity of type " << i << " position:\n  "
                   << near[0] << " " << near[1] << " " << near[2] << "\n";
         rval = pFacet->getEntClosestPt(this_gent, near[0], near[1], near[2],
-            &on[0], &on[1], &on[2]);
-        CHECK("Failed to get closest point on entity.");
+            &on[0], &on[1], &on[2]);MB_CHK_SET_ERR(rval,"Failed to get closest point on entity.");
+
         std::cout << "   close by:  "
                           << on[0] << " " << on[1] << " " << on[2] << "\n";
       }
@@ -380,8 +346,7 @@ ErrorCode normals_test(FBEngine * pFacet)
 {
   int i;
   EntityHandle root_set;
-  ErrorCode rval = pFacet->getRootSet(&root_set);
-  CHECK( "ERROR : getRootSet failed!" );
+  ErrorCode rval = pFacet->getRootSet(&root_set);MB_CHK_SET_ERR(rval, "ERROR : getRootSet failed!" );
 
   int top = 0;//iBase_VERTEX;
   int num_test_top = 4; //iBase_ALL_TYPES;
@@ -391,8 +356,7 @@ ErrorCode normals_test(FBEngine * pFacet)
   //
   for (i = top; i < num_test_top; i++) {
     Range gentities;
-    rval = pFacet->getEntities(root_set, i, gentities);
-    CHECK("Failed to get gentities in eval tests.");
+    rval = pFacet->getEntities(root_set, i, gentities);MB_CHK_SET_ERR(rval,"Failed to get gentities in eval tests.");
 
     gentity_vectors[i].resize(gentities.size());
     std::copy(gentities.begin(), gentities.end(), gentity_vectors[i].begin());
@@ -406,14 +370,12 @@ ErrorCode normals_test(FBEngine * pFacet)
     for (vit = gentity_vectors[i].begin(); vit != gentity_vectors[i].end(); ++vit) {
       EntityHandle this_gent = *vit;
       rval = pFacet->getEntBoundBox(this_gent, &min[0], &min[1], &min[2],
-          &max[0], &max[1], &max[2]);
-      CHECK("Failed to get bounding box of entity.");
+          &max[0], &max[1], &max[2]);MB_CHK_SET_ERR(rval,"Failed to get bounding box of entity.");
 
       rval = pFacet->getEntNrmlXYZ(this_gent, (max[0] + min[0]) / 2, (max[1]
           + min[1]) / 2, (max[2] + min[2]) / 2, &normal[0], &normal[1],
-          &normal[2]);
+          &normal[2]);MB_CHK_SET_ERR(rval,"Failed to get normal to the closest point.");
 
-      CHECK("Failed to get normal to the closest point.");
       std::cout << " entity of type " << i << " closest normal to center:\n  "
           << normal[0] << " " << normal[1] << " " << normal[2] << "\n";
     }
@@ -427,14 +389,12 @@ ErrorCode ray_test(FBEngine * pFacet)
 {
 
   EntityHandle root_set;
-  ErrorCode rval = pFacet->getRootSet(&root_set);
-  CHECK( "ERROR : getRootSet failed!" );
+  ErrorCode rval = pFacet->getRootSet(&root_set);MB_CHK_SET_ERR(rval, "ERROR : getRootSet failed!" );
 
   int top = 2; //  iBase_FACE;
 
   Range faces;
-  rval = pFacet->getEntities(root_set, top, faces);
-  CHECK("Failed to get faces in ray_test.");
+  rval = pFacet->getEntities(root_set, top, faces);MB_CHK_SET_ERR(rval,"Failed to get faces in ray_test.");
 
   // check only the first face
 
@@ -444,8 +404,7 @@ ErrorCode ray_test(FBEngine * pFacet)
   EntityHandle first_face = faces[0];
 
   rval = pFacet->getEntBoundBox(first_face, &min[0], &min[1], &min[2], &max[0],
-      &max[1], &max[2]);
-  CHECK("Failed to get bounding box of entity.");
+      &max[1], &max[2]);MB_CHK_SET_ERR(rval,"Failed to get bounding box of entity.");
 
   // assume that the ray shot from the bottom of the box (middle) is a pretty good candidate
   // in z direction
@@ -453,13 +412,13 @@ ErrorCode ray_test(FBEngine * pFacet)
   std::vector<EntityHandle> intersect_entity_handles;
   std::vector<double> intersect_coords;
   std::vector<double> param_coords;
+
   rval = pFacet->getPntRayIntsct(x, y, z, // shot from
       0., 0., 1., // direction
       intersect_entity_handles,
       /*iBase_INTERLEAVED,*/
-      intersect_coords, param_coords);
+      intersect_coords, param_coords);MB_CHK_SET_ERR(rval,"Failed to find ray intersections points ");
 
-  CHECK("Failed to find ray intersections points ");
   for (unsigned int i = 0; i < intersect_entity_handles.size(); i++) {
 
     std::cout << " entity of type " << pFacet->moab_instance ()->type_from_handle(intersect_entity_handles[i]) <<
@@ -479,13 +438,13 @@ ErrorCode split_test(Interface * mb, FBEngine * pFacet)
 
   EntityHandle root_set;
   ErrorCode rval = pFacet->getRootSet(&root_set);
-  CHECK( "ERROR : getRootSet failed!" );
+  MB_CHK_SET_ERR(rval, "ERROR : getRootSet failed!" );
 
   int top = 2; //  iBase_FACE;
 
   Range faces;
   rval = pFacet->getEntities(root_set, top, faces);
-  CHECK("Failed to get faces in split_test.");
+  MB_CHK_SET_ERR(rval,"Failed to get faces in split_test.");
 
   // check only the first face
 
@@ -529,10 +488,8 @@ ErrorCode split_test(Interface * mb, FBEngine * pFacet)
   }
 
   EntityHandle newFace;// first test is with closed surface
-  rval = pFacet->split_surface_with_direction(first_face, xyz, direction,   /*closed*/1, min_dot, newFace);
+  rval = pFacet->split_surface_with_direction(first_face, xyz, direction,   /*closed*/1, min_dot, newFace);MB_CHK_SET_ERR(rval, "ERROR : splitting surface failed!" );
 
-  if (rval!=MB_SUCCESS)
-    return rval;
   // save a new database
   pFacet->delete_smooth_tags();
   // get a new gtt tool
@@ -542,15 +499,14 @@ ErrorCode split_test(Interface * mb, FBEngine * pFacet)
   GeomTopoTool * duplicate = NULL;
   std::vector<EntityHandle> gents;
   gents.push_back(newFace);
-  rval = gtt->duplicate_model(duplicate, &gents);
-  CHECK("Failed to extract surface.");
+  rval = gtt->duplicate_model(duplicate, &gents);MB_CHK_SET_ERR(rval,"Failed to extract surface.");
+
   EntityHandle newRootSet = duplicate->get_root_model_set() ;
   delete pFacet;
   pFacet = NULL;// try not to write the obb tree
-  rval = mb->write_file(filename_out.c_str(), NULL, NULL, &newRootSet, 1);
+  rval = mb->write_file(filename_out.c_str(), NULL, NULL, &newRootSet, 1);MB_CHK_SET_ERR(rval, "ERROR : writing mesh failed!" );
 
   delete duplicate;
-
   return rval;
 }
 
@@ -558,11 +514,9 @@ ErrorCode check_split(Interface * mb)
 {
   // check loading the file in an empty db
   //delete pFacet;// should clean up the FBEngine
-  ErrorCode rval = mb->delete_mesh();
-  CHECK( "ERROR : delete mesh failed!" );
+  ErrorCode rval = mb->delete_mesh();MB_CHK_SET_ERR(rval, "ERROR : delete mesh failed!" );
 
-  rval = mb->load_file(filename_out.c_str());
-  CHECK( "ERROR : can't load modified file!" );
+  rval = mb->load_file(filename_out.c_str());MB_CHK_SET_ERR(rval, "ERROR : can't load modified file!" );
 
   FBEngine * pFacet = new FBEngine(mb, NULL, true);// smooth facetting, no OBB tree passed
 
@@ -570,8 +524,7 @@ ErrorCode check_split(Interface * mb)
 
   // should the init be part of constructor or not?
   // this is where the obb tree is constructed, and smooth faceting initialized, too.
-  rval = pFacet->Init();
-  CHECK("failed to initialize smoothing");
+  rval = pFacet->Init();MB_CHK_SET_ERR(rval,"failed to initialize smoothing");
 
   std::cout << "root set test: ";
   rval = root_set_test(pFacet);
@@ -611,28 +564,24 @@ ErrorCode split_quads_test()
   Core mbcore;
   Interface * mb = &mbcore;
 
-  ErrorCode  rval = mb->load_file(quads_file.c_str());
-  CHECK("failed to load quads file");
+  ErrorCode  rval = mb->load_file(quads_file.c_str());MB_CHK_SET_ERR(rval,"failed to load quads file");
+
   FBEngine * pFacet = new FBEngine(mb, NULL, true);
 
-  rval = pFacet->Init();
-  CHECK("failed to initialize smoothing");
+  rval = pFacet->Init();MB_CHK_SET_ERR(rval,"failed to initialize smoothing");
 
   EntityHandle root_set;
-  rval = pFacet->getRootSet(&root_set);
-  CHECK( "ERROR : getRootSet failed!" );
+  rval = pFacet->getRootSet(&root_set);MB_CHK_SET_ERR(rval, "ERROR : getRootSet failed!" );
   int top = 2; //  iBase_FACE;
 
   Range faces;
-  rval = pFacet->getEntities(root_set, top, faces);
-  CHECK("Failed to get faces in split_test.");
+  rval = pFacet->getEntities(root_set, top, faces);MB_CHK_SET_ERR(rval,"Failed to get faces in split_test.");
 
   if (faces.size() !=2)
   {
     std::cout << "num faces after loading quads model:" << faces.size() << "\n";
     return MB_FAILURE;//
   }
-
 
   // multiple tests
   std::cout << "gentity set test: ";
@@ -661,8 +610,7 @@ ErrorCode split_quads_test()
   delete pFacet;
   pFacet= NULL;
   std::string spl_file="q.split.h5m";
-  rval = mb->write_file(spl_file.c_str(), 0, 0, &root_set, 1);
-  CHECK("can't write result file");
+  rval = mb->write_file(spl_file.c_str(), 0, 0, &root_set, 1);MB_CHK_SET_ERR(rval,"can't write result file");
 
   if (!keep_output)
   {
