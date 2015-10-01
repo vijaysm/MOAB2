e8917adb0e9456df4ff734f3ae7b8b8a75a921d8
Nico Schlömer <nico.schloemer@gmail.com>
2015-10-01 10:26:16 +0200

    don't require METIS_DIR, PARMETIS_DIR to be set
    Before this commit, CMake would fail if either of METIS_DIR, PARMETIS_DIR are
    not set even when METIS and PARMETIS have been found.
    
    This commit makes the CMake configuration pass if METIS and PARMETIS have been
    found independently of whether (PAR)METIS_DIR is set.

--- config/FindMetis.cmake
+++ config/FindMetis.cmake
@@ -49,6 +49,8 @@ mark_as_advanced(
 )
 
 include (FindPackageHandleStandardArgs)
-find_package_handle_standard_args (Metis "Metis not found, check environment variables METIS_DIR"
-  METIS_DIR METIS_INCLUDES METIS_LIBRARIES)
-
+find_package_handle_standard_args (
+  Metis "Metis not found, check environment variables METIS_DIR"
+  METIS_INCLUDES
+  METIS_LIBRARIES
+  )
