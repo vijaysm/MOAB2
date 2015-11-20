376be1738977a878bea80b2902bd56a51284e6c5
Iulian Grindeanu <iulian@mcs.anl.gov>
2015-11-20 12:35:08 -0600

    we need this change for windows only
    we do not want to propagate for linux yet

--- src/TupleList.cpp
+++ src/TupleList.cpp
@@ -553,8 +553,10 @@ ErrorCode TupleList::sort(uint key, TupleList::buffer *buf)
       umax_2(Ulong_size,real_size));
   unsigned data_size =
       key >= mi ? sizeof(SortData<long> ) : sizeof(SortData<uint> );
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32)
   if (key>=mi+ml)
     data_size = sizeof(SortData<Ulong> );
+#endif 
 
   uint work_min = n * umax_2(2*data_size,sizeof(sint)+width);
   uint *work;
