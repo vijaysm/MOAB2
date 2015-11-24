sigma-toolkit/moab-migration

shared: export symbols used within the build
Symbols used within the tree are now explicitly exported from the
library. This is the first step to getting shared build support on
Windows. Tested by adding -fvisibility=hidden to the build flags.

tools/refiner/EdgeSizeSimpleImplicit.hpp
@@ -30,11 +30,13 @@
 #ifndef MOAB_EDGE_SIZE_SIMPLE_IMPLICIT_HPP
 #define MOAB_EDGE_SIZE_SIMPLE_IMPLICIT_HPP
 
+#include "moabrefiner_export.h"
+
 #include "EdgeSizeEvaluator.hpp"
 
 namespace moab {
 
-class EdgeSizeSimpleImplicit : public EdgeSizeEvaluator
+class MOABREFINER_EXPORT EdgeSizeSimpleImplicit : public EdgeSizeEvaluator
 {
 public:
   /// Construct an evaluator.

tools/refiner/MeshRefiner.hpp
@@ -26,7 +26,8 @@
 #ifndef MOAB_MESH_REFINER_HPP
 #define MOAB_MESH_REFINER_HPP
 
-#include "moab/MOABConfig.h"
+#include "moabrefiner_export.h"
+
 #include "moab/Compiler.hpp" // for MB_DLL_EXPORT
 #include "moab/Range.hpp"
 
@@ -40,7 +41,7 @@ class ParallelComm;
 class RefinerTagManager;
 class MeshOutputFunctor;
 
-class MeshRefiner
+class MOABREFINER_EXPORT MeshRefiner
 {
 public:
   MeshRefiner( Interface* imesh, Interface* omesh );

tools/refiner/SimplexTemplateRefiner.hpp
@@ -29,6 +29,8 @@
 #ifndef MB_SIMPLEX_TEMPLATE_REFINER_HPP
 #define MB_SIMPLEX_TEMPLATE_REFINER_HPP
 
+#include "moabrefiner_export.h"
+
 #include "EntityRefiner.hpp"
 #include "SimplexTemplateTagAssigner.hpp"
 
@@ -38,7 +40,7 @@ namespace moab {
 
 class RefinerTagManager;
 
-class SimplexTemplateRefiner : public EntityRefiner
+class MOABREFINER_EXPORT SimplexTemplateRefiner : public EntityRefiner
 {
 public:
   SimplexTemplateRefiner();

vijaysm/MOAB2

tools/refiner/EdgeSizeSimpleImplicit.hpp
@@ -30,11 +30,13 @@
 #ifndef MOAB_EDGE_SIZE_SIMPLE_IMPLICIT_HPP
 #define MOAB_EDGE_SIZE_SIMPLE_IMPLICIT_HPP
 
+#include "refiner_export.h"
+
 #include "EdgeSizeEvaluator.hpp"
 
 namespace moab {
 
-class EdgeSizeSimpleImplicit : public EdgeSizeEvaluator
+class REFINER_EXPORT EdgeSizeSimpleImplicit : public EdgeSizeEvaluator
 {
 public:
   /// Construct an evaluator.

tools/refiner/MeshRefiner.hpp
@@ -26,7 +26,8 @@
 #ifndef MOAB_MESH_REFINER_HPP
 #define MOAB_MESH_REFINER_HPP
 
-#include "moab/MOABConfig.h"
+#include "refiner_export.h"
+
 #include "moab/Compiler.hpp" // for MB_DLL_EXPORT
 #include "moab/Range.hpp"
 
@@ -40,7 +41,7 @@ class ParallelComm;
 class RefinerTagManager;
 class MeshOutputFunctor;
 
-class MeshRefiner
+class REFINER_EXPORT MeshRefiner
 {
 public:
   MeshRefiner( Interface* imesh, Interface* omesh );

tools/refiner/SimplexTemplateRefiner.hpp
@@ -29,6 +29,8 @@
 #ifndef MB_SIMPLEX_TEMPLATE_REFINER_HPP
 #define MB_SIMPLEX_TEMPLATE_REFINER_HPP
 
+#include "refiner_export.h"
+
 #include "EntityRefiner.hpp"
 #include "SimplexTemplateTagAssigner.hpp"
 
@@ -38,7 +40,7 @@ namespace moab {
 
 class RefinerTagManager;
 
-class SimplexTemplateRefiner : public EntityRefiner
+class REFINER_EXPORT SimplexTemplateRefiner : public EntityRefiner
 {
 public:
   SimplexTemplateRefiner();